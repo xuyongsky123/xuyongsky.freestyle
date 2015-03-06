6586df3cc2033a6379b30869c1df027792595436
987864207 <987864207@qq.com>
2015-03-06 08:40:28 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/index.php

--- index.php
+++ index.php
@@ -26,7 +26,41 @@
 				//到最后一屏时循环
 				continuousVertical: true,
 
-				
+				//菜单栏
+				anchors: ['page1', 'page2', 'page3', 'page4'],
+				menu: '#menu',
+
+				//屏幕内容呈现动态（回调函数）
+				afterLoad: function(anchorLink, index){
+					if(index == 2){
+						$('.section2').find('p').delay(500).animate({
+							left: '0'
+						}, 1500, 'easeOutExpo');
+					}
+					if(index == 3){
+						$('.section3').find('p').delay(500).animate({
+							bottom: '0'
+						}, 1500, 'easeOutExpo');
+					}
+					if(index == 4){
+						$('.section4').find('p').fadeIn(2000);
+					}
+				},
+				onLeave: function(index, direction){
+					if(index == '2'){
+						$('.section2').find('p').delay(500).animate({
+							left: '-120%'
+						}, 1500, 'easeOutExpo');
+					}
+					if(index == '3'){
+						$('.section3').find('p').delay(500).animate({
+							bottom: '-120%'
+						}, 1500, 'easeOutExpo');
+					}
+					if(index == '4'){
+						$('.section4').find('p').fadeOut(2000);
+					}
+				}
 
 				//项目导航栏
 				//'navigation': true,
