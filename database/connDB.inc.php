33684fac4aacc97b2d8370d69af4cfb79a91207a
987864207 <987864207@qq.com>
2015-03-06 09:53:40 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/database/connDB.inc.php

--- database/connDB.inc.php
+++ database/connDB.inc.php
@@ -1,5 +1,5 @@
 <?php
-header("Content-Type:text/html;charset=utf-8");
+	header("Content-Type:text/html;charset=utf-8");
 	require("ConnDB.class.inc.php");
 
 	$host=SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT;
