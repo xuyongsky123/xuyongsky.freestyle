79d2acd7089d58196189e6ccb1d79abf7f60362f
sae-web <sae-web@sina.com.cn>
2015-03-06 09:16:03 +0000

    auto versioning

---
+++ module_blog/show_article.php
@@ -0,0 +1,22 @@
+<?php 
+	
+	//session_start();
+	require("../database/connDB.inc.php");
+	
+	
+	if(isset($_SESSION['id'])){
+		
+		$user=$_SESSION['username'];
+		$sql="SELECT * FROM USER_ARTICLE WHERE username = '$user' ";
+		$array=$operatedb->Execsql($sql,$conn);
+		
+		foreach($array as $item){
+				
+			echo "<li><a href='../module_blog/blog.php?filename=".$item['filename']."'>".$item['filename']."</a></li>";
+				
+		} 	
+	}else{
+		echo "这里将输出公共bolg，不属于具体私人的...";	
+	}	
+	
+?>
