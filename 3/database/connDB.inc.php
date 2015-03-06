760b188226130a291ee21e00bb9008e683c63be6
987864207@qq.com <987864207@qq.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-06 09:04:40 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/database/connDB.inc.php

--- 3/database/connDB.inc.php
+++ 3/database/connDB.inc.php
@@ -2,11 +2,11 @@
 	require("ConnDB.class.inc.php");
 
 	$host=SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT;
-	$user=SAE_MYSQL_USER;
-	$pwd=SAE_MYSQL_PASS;
+	$dbuser=SAE_MYSQL_USER;
+	$dbpwd=SAE_MYSQL_PASS;
 	$dbname=SAE_MYSQL_DB;
 
-	$connClass=new ConnDB("mysql","$host","$user","$pwd","$dbname");
+	$connClass=new ConnDB("mysql","$host","$dbuser","$dbpwd","$dbname");
 	$operatedb=new OperateDB();
 	$conn=$connClass->GetConn();
 ?>
