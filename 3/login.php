9605041644718f4480cbd607d20e6d293e15f9a9
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-09 03:04:50 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/login.php

--- 3/login.php
+++ 3/login.php
@@ -1,12 +1,7 @@
 <?php 
 	header("Content-Type=text/html;charset=UTF-8");
     
-    $path='./session_files/';
-    if(!is_dir($path)){
-        mkdir($path);
-    }
-    session_save_path($path);
-
+    
 	session_start();
 	if(isset($_GET['action'])){
 		//注销登录
