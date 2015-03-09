238fbb62e2796d60f87c010b829fc6336bc5ac75
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-09 09:15:07 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_blog/handle_upload.php

--- 3/module_blog/handle_upload.php
+++ 3/module_blog/handle_upload.php
@@ -12,17 +12,17 @@
 		
 		if($extName=='txt'){
 			
-            $dest_dir="/uploads/files";
+            $dest_dir="../uploads/files";
 			$dest=$dest_dir.'/'.$filename;
 			
 		}else if($extName=='jpg'){
 			
-            $dest_dir="/uploads/images";
+            $dest_dir="../uploads/images";
 			$dest=$dest_dir.'/'.$filename;
 			
 		}else{
 			
-            $dest_dir="/uploads/other_files";
+            $dest_dir="../uploads/other_files";
 			$dest=$dest_dir.'/'.$filename;
 				
 		}
