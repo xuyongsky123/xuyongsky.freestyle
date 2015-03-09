5d6edd23ea5592a17254a8576b6c89fc13132a28
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-09 03:31:38 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/register.php

--- 3/register.php
+++ 3/register.php
@@ -1,7 +1,6 @@
 <?php 
-	header("Content-Type=text/html;charset=utf-8");
-
 	session_start();
+	header("Content-Type=text/html;charset=utf-8");
 	
 	if(!isset($_POST['submit'])){
 		exit('非法访问！！！');	
