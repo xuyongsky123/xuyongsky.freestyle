24795482838ec1d8b926551c271bd5d9eb85d7c7
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-09 07:06:17 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/check_login.php

--- 3/check_login.php
+++ 3/check_login.php
@@ -1,23 +1,5 @@
 <?php 
-/*if(isset($_SESSION['id'])){
-		echo '<span>';
-		echo '欢迎您，'.$_SESSION['username'].'&nbsp';
-		echo '</span>';
-		echo '<span>|</span>';
-		echo '<span>';
-		echo '<a href="./login.php?action=logout">&nbsp注销</a>';
-		echo '</span>';
-    }else{
-		echo '<span>';
-		echo '<a href="./login.html">登录&nbsp</a>';
-		echo '</span>';
-		echo '<span>|</span>';
-		echo '<span>';
-		echo '<a href="./register.html">&nbsp注册</a>';
-		echo '</span>';
-	}*/
-	$mmc=memcache_init();
-	if(isset(memcache_get($mmc,'id')){
+if(isset($_SESSION['id'])){
 		echo '<span>';
 		echo '欢迎您，'.$_SESSION['username'].'&nbsp';
 		echo '</span>';
