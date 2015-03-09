37b55157b2544383a303c7156368978f1f37e126
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-09 03:33:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/register.php

--- 3/register.php
+++ 3/register.php
@@ -32,7 +32,7 @@
 						$_SESSION['username']=$user;
 						$_SESSION['id']=$result_item[0]['id'];
 						
-                        header('refresh:0;url=./index.php');
+                        header('Location:url=./index.php');
 						exit;	
 					}
 				}else{
