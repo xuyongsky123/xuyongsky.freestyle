079445458e9f98a10c37b81647d0350571af753e
sae-web <sae-web@sina.com.cn>
2015-03-09 03:00:15 +0000

    auto versioning

---
+++ check_login.php
@@ -0,0 +1,19 @@
+<?php 
+	if(isset($_SESSION['id'])){
+		echo '<span>';
+		echo '欢迎您，'.$_SESSION['username'].'&nbsp';
+		echo '</span>';
+		echo '<span>|</span>';
+		echo '<span>';
+		echo '<a href="./login.php?action=logout">&nbsp注销</a>';
+		echo '</span>';
+	}else{
+		echo '<span>';
+		echo '<a href="./login.html">登录&nbsp</a>';
+		echo '</span>';
+		echo '<span>|</span>';
+		echo '<span>';
+		echo '<a href="./register.html">&nbsp注册</a>';
+		echo '</span>';
+	}
+?>
