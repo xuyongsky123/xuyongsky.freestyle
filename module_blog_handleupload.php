bfab8a58d91aad11e9c448cdeb89d728a06c9221
xuyongsky123 <xuyongsky123@163.com>
2015-03-10 07:42:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_blog_handleupload.php

--- module_blog_handleupload.php
+++ module_blog_handleupload.php
@@ -12,17 +12,17 @@
 		
 		if($extName=='txt'){
 			
-            $dest_dir="../uploads/files";
+            $dest_dir="./uploads/files";
 			$dest=$dest_dir.'/'.$filename;
 			
 		}else if($extName=='jpg'){
 			
-            $dest_dir="../uploads/images";
+            $dest_dir="./uploads/images";
 			$dest=$dest_dir.'/'.$filename;
 			
 		}else{
 			
-            $dest_dir="../uploads/other_files";
+            $dest_dir="./uploads/other_files";
 			$dest=$dest_dir.'/'.$filename;
 				
 		}
@@ -30,12 +30,12 @@
 		move_uploaded_file($file['tmp_name'],$dest);
 		
 		echo "<script>javascript:alert('上传成功')</script>";
-		header('refresh:0;url=/module_blog/blog.php');
+		header('refresh:0;url=./module_blog_blog.php');
 		exit;
 		
 	}else{
 		echo "<script>javascript:alert('上传失败')</script>";	
-		header('refresh:0;url=/module_blog/blog.php');
+		header('refresh:0;url=./module_blog_blog.php');
 		exit;	
 	}
 ?>
