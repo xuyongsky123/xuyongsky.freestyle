8ed1e04375f34c0a6138a84a30ba1f2ef9e956cd
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-10 08:26:15 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_blog_showarticle.php

--- 3/module_blog_showarticle.php
+++ 3/module_blog_showarticle.php
@@ -7,12 +7,16 @@
 		$user=$_SESSION['username'];
 		$sql="select * from user_article where username = '$user' ";
 		$array=$operatedb->Execsql($sql,$conn);
+        
+        if($array){
+        	foreach($array as $item){
+				echo "<li><a href='./module_blog_blog.php?filename=".$item['filename']."'>".$item['filename']."</a></li>";
+			}
+        }else{
+        	echo "您暂时还没有写过文章..."
+        }
 		
-		foreach($array as $item){
-				
-			echo "<li><a href='./module_blog_blog.php?filename=".$item['filename']."'>".$item['filename']."</a></li>";
-				
-		} 	
+		 	
 	}else{
 		echo "这里将输出公共bolg，不属于具体私人的...";	
 	}	
