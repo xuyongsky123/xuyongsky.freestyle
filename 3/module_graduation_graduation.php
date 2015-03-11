931048f061d9aa16154f5eaf5551c1cff2312126
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-11 09:38:15 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_graduation.php

--- 3/module_graduation_graduation.php
+++ 3/module_graduation_graduation.php
@@ -10,6 +10,21 @@
 			<div class="st-pusher">
 				<div class="st-content"><!-- this is the wrapper for the content -->
 					<div class="st-content-inner"><!-- extra div for emulating position:fixed of the menu -->
+                        <section class="col-1-1">
+                            <h2>Playground</h2>
+                            <div class="playground grid">
+                              <div class="col-1-1 viewport">
+                                  <div id="title_content" class="tlt">
+                                    <ul class="texts" style="display: none">
+                                      <li>还在为自己的毕业设计而担忧吗？赶快行动吧~~</li>
+                                    </ul>
+                                  </div>
+                                  <div id="st-trigger-effects" class="column">
+                                     <button data-effect="st-effect-12">点我~开始自己的毕业设计制作之旅吧~</button>
+                                  </div>	
+                              </div>
+                            </div>
+                        </section>
 						<div class="main clearfix fixstyle">
 							<div id="title_content">还在为自己的毕业设计而担忧吗？赶快行动吧~~</div>
                         	<div id="st-trigger-effects" class="column">
