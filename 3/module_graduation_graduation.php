90f8fe8ad1fe5ff823171f4d638c995aea2024df
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-11 06:52:54 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_graduation.php

--- 3/module_graduation_graduation.php
+++ 3/module_graduation_graduation.php
@@ -12,7 +12,7 @@
 						<div>content is here...</div>
 						<div class="main clearfix">
 							<div id="st-trigger-effects" class="column">
-								<button data-effect="xuyong">毕业设计制作</button>
+								<button data-effect="st-effect-12">毕业设计制作</button>
 							</div>
 						</div><!-- /main -->
 					</div><!-- /st-content-inner -->
