4e3c1e3c09c4b85d1cc4bdb9fab596960837acda
xuyongsky123 <xuyongsky123@163.com>
2015-03-11 06:08:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_templethead.php

--- module_graduation_templethead.php
+++ module_graduation_templethead.php
@@ -2,9 +2,9 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 <title>毕业设计专栏</title>
-<meta name="description" content="Sidebar Transitions: Transition effects for off-canvas views" />
-<meta name="keywords" content="transition, off-canvas, navigation, effect, 3d, css3, smooth" />
-<meta name="author" content="Codrops" />
+<meta name="description" content="graduation project" />
+<meta name="keywords" content="graduation project,graduation" />
+<!--<meta name="author" content="Codrops" />-->
 <link rel="stylesheet" type="text/css" href="./module_graduation_css/normalize.css" />
 <link rel="stylesheet" type="text/css" href="./module_graduation_css/demo.css" />
 <link rel="stylesheet" type="text/css" href="./module_graduation_css/component.css" />
