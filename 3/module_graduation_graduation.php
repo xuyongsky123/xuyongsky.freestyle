ad9b3beb8c417a09d86a6db0712f5daff370f129
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-11 09:21:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_graduation.php

--- 3/module_graduation_graduation.php
+++ 3/module_graduation_graduation.php
@@ -4,17 +4,18 @@
     	<?php include("./module_graduation_templethead.php") ?>
     </head>
     <body>
-		<div id="st-container" class="st-container">
-			<?php include("./module_graduation_templetmenu.php") ?>
-			<div class="st-pusher">
-				<div class="st-content"><!-- this is the wrapper for the content -->
-					<div class="st-content-inner"><!-- extra div for emulating position:fixed of the menu -->
-						<div class="main clearfix fixstyle">
+        <div class="main clearfix fixstyle">
 							<div id="title_content">还在为自己的毕业设计而担忧吗？赶快行动吧~~</div>
                         	<div id="st-trigger-effects" class="column">
 								<button data-effect="st-effect-12">点我~开始自己的毕业设计制作之旅吧~</button>
 							</div>
 						</div><!-- /main -->
+		<div id="st-container" class="st-container">
+			<?php include("./module_graduation_templetmenu.php") ?>
+			<div class="st-pusher">
+				<div class="st-content"><!-- this is the wrapper for the content -->
+					<div class="st-content-inner"><!-- extra div for emulating position:fixed of the menu -->
+						
 					</div><!-- /st-content-inner -->
 				</div><!-- /st-content -->
 			</div><!-- /st-pusher -->
