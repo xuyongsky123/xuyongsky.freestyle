7044804ebf96471df686e7cf802ec68db6cc0876
xuyongsky123 <xuyongsky123@163.com>
2015-03-11 10:40:48 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_requirement.php

--- module_graduation_requirement.php
+++ module_graduation_requirement.php
@@ -2,3 +2,14 @@
 	header("Content-Type:text/html;charset=utf-8;");
 	echo "<div style='background:lightgreen;font-size:2rem;margin-top:15%;padding:50px;text-align:center;'>功能正在开发中...</div>";
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+        <link href="./css/module_graduation_css/module_graduation_style.css" rel="stylesheet">
+    </head>
+    <body>
+    	<div id="graduation_login">
+            <?php require("./check_login.php"); ?>
+        </div>
+    </body>
+</html>
