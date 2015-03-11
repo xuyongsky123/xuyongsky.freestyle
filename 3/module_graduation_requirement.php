2e7b5d8ff198a837aeabe8e09cccfd38f0f46578
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-11 10:06:44 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_requirement.php

--- 3/module_graduation_requirement.php
+++ 3/module_graduation_requirement.php
@@ -1,4 +1,4 @@
 <?php 
-header("Content-Type:text/html;charset=utf-8;")
+	header("Content-Type:text/html;charset=utf-8;");
 	echo "<div style='background:lightgreen;font-size:2rem;margin-top:15%;text-align:center;'>功能正在开发中...</div>";
 ?>
