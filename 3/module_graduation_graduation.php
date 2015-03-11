e6323a953e7c599a6d8cf48fc90992c0efff4203
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-11 09:38:21 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_graduation.php

--- 3/module_graduation_graduation.php
+++ 3/module_graduation_graduation.php
@@ -25,12 +25,6 @@
                               </div>
                             </div>
                         </section>
-						<div class="main clearfix fixstyle">
-							<div id="title_content">还在为自己的毕业设计而担忧吗？赶快行动吧~~</div>
-                        	<div id="st-trigger-effects" class="column">
-								<button data-effect="st-effect-12">点我~开始自己的毕业设计制作之旅吧~</button>
-							</div>
-						</div><!-- /main -->
 					</div><!-- /st-content-inner -->
 				</div><!-- /st-content -->
 			</div><!-- /st-pusher -->
