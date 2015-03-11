02efe197535cf2f6b53b5092d338f67a3f44ee5e
xuyongsky123 <xuyongsky123@163.com>
2015-03-11 06:12:57 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_graduation.php

--- module_graduation_graduation.php
+++ module_graduation_graduation.php
@@ -1,2 +1,37 @@
-<?php include("./module_graduation_templethead.php") ?>
+<!DOCTYPE html>
+<html>
+    <head>
+    	<?php include("./module_graduation_templethead.php") ?>
+    </head>
+    <body>
+		<div id="st-container" class="st-container">
+			<nav class="st-menu st-effect-12" id="menu-12">
+				<h2 class="icon icon-stack">Sidebar</h2>
+				<ul>
+					<li><a class="icon icon-data" href="#">Data Management</a></li>
+					<li><a class="icon icon-location" href="#">Location</a></li>
+					<li><a class="icon icon-study" href="#">Study</a></li>
+					<li><a class="icon icon-photo" href="#">Collections</a></li>
+					<li><a class="icon icon-wallet" href="#">Credits</a></li>
+				</ul>
+			</nav>
+
+			<div class="st-pusher">
+				<div class="st-content"><!-- this is the wrapper for the content -->
+					<div class="st-content-inner"><!-- extra div for emulating position:fixed of the menu -->
+						<div>content is here...</div>
+						<div class="main clearfix">
+							<div id="st-trigger-effects" class="column">
+								<button data-effect="st-effect-12">效果十二</button>
+							</div>
+						</div><!-- /main -->
+					</div><!-- /st-content-inner -->
+				</div><!-- /st-content -->
+			</div><!-- /st-pusher -->
+		</div><!-- /st-container -->
+		<script src="js/classie.js"></script>
+		<script src="js/sidebarEffects.js"></script>
+	</body>
+</html>
+
 <?php include("./module_graduation_templeteffect.php") ?>
