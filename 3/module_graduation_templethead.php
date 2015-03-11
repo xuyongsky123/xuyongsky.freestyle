3897bf6436f43d070a7712421cef06417a602e27
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-11 06:04:07 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_templethead.php

---
+++ 3/module_graduation_templethead.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<title>边栏过渡效果（Sidebar Transitions）</title>
+<meta name="description" content="Sidebar Transitions: Transition effects for off-canvas views" />
+<meta name="keywords" content="transition, off-canvas, navigation, effect, 3d, css3, smooth" />
+<meta name="author" content="Codrops" />
+<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+<link rel="stylesheet" type="text/css" href="css/demo.css" />
+<link rel="stylesheet" type="text/css" href="css/component.css" />
+<script src="./module_graduation_js/modernizr.custom.js"></script>
