df27b52a0f1079a1dd4b8dcca33a1d746e2be252
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-11 10:34:30 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_document.php

--- 3/module_graduation_document.php
+++ 3/module_graduation_document.php
@@ -2,3 +2,16 @@
 	header("Content-Type:text/html;charset=utf-8;");
 	echo "<div style='background:lightgreen;font-size:2rem;margin-top:15%;padding:50px;text-align:center;'>功能正在开发中...</div>";
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+    	<?php include("./module_graduation_templethead.php") ?>
+        <link href="./css/module_graduation_css/module_graduation_style.css" rel="stylesheet">
+        <link href="./css/module_graduation_css/home_page/animate.css" rel="stylesheet">
+    </head>
+    <body>
+    	<div id="graduation_login">
+            <?php require("./check_login.php"); ?>
+        </div>
+    </body>
+</html>
