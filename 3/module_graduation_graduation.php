239a689f7969b0c2c3bac53f0af3f71d3c1b75d9
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-11 09:43:18 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_graduation.php

--- 3/module_graduation_graduation.php
+++ 3/module_graduation_graduation.php
@@ -8,7 +8,8 @@
 		<div id="st-container" class="st-container">
 			<?php include("./module_graduation_templetmenu.php") ?>
 			<div class="st-pusher">
-                <section class="col-1-1">
+				<div class="st-content"><!-- this is the wrapper for the content -->
+                    <section class="col-1-1">
                             <h2>Playground</h2>
                             <div class="playground grid">
                               <div class="col-1-1 viewport">
@@ -45,7 +46,6 @@
                               </div>
                             </div>
                         </section>
-				<div class="st-content"><!-- this is the wrapper for the content -->
 					<div class="st-content-inner"><!-- extra div for emulating position:fixed of the menu -->
 						<div class="main clearfix fixstyle">
 							<div id="title_content">还在为自己的毕业设计而担忧吗？赶快行动吧~~</div>
