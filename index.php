d5953ef0243e6badb795e4c047f4f4cb7c59b9b6
xuyongsky123 <xuyongsky123@163.com>
2015-03-11 05:23:48 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/index.php

--- index.php
+++ index.php
@@ -123,8 +123,8 @@
 		<div class="section section1">
 			<h3>请选择：</h3>
             
-			<div class="slide"><h3><a href="./module_blog_blog.php">Take a look!&nbsp;&nbsp;毕业设计专栏</a></h3></div>
-			<div class="slide"><h3><a href="./module_blog_blog.php">Take a look!&nbsp;&nbsp;博客文章专栏</a></h3></div>
+			<div class="slide"><h3><a href="./module_blog_blog.php">Make Graduation Project!&nbsp;&nbsp;毕业设计专栏</a></h3></div>
+			<div class="slide"><h3><a href="./module_blog_blog.php">Write blog!&nbsp;&nbsp;博客文章专栏</a></h3></div>
 			<div class="slide"><h3>功能模块示例一</h3></div>
 			<div class="slide"><h3>功能模块示例二</h3></div>
 		</div>
