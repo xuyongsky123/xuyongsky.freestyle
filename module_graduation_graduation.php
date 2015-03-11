c360d4930a34e1b231a761151593e858b61f55b7
xuyongsky123 <xuyongsky123@163.com>
2015-03-11 08:02:37 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_graduation.php

--- module_graduation_graduation.php
+++ module_graduation_graduation.php
@@ -10,9 +10,10 @@
 				<div class="st-content"><!-- this is the wrapper for the content -->
 					<div class="st-content-inner"><!-- extra div for emulating position:fixed of the menu -->
 						<div id="title_content">还在为自己的毕业设计而担忧吗？赶快行动吧~~</div>
+                        <button data-effect="st-effect-12">毕业设计制作</button>
 						<div class="main clearfix">
 							<div id="st-trigger-effects" class="column">
-								<button data-effect="st-effect-12">毕业设计制作</button>
+								
 							</div>
 						</div><!-- /main -->
 					</div><!-- /st-content-inner -->
