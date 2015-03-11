54c16aafc971cab92247779cff22b192b578afab
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-11 09:59:38 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_templetmenu.php

--- 3/module_graduation_templetmenu.php
+++ 3/module_graduation_templetmenu.php
@@ -1,7 +1,7 @@
 <nav class="st-menu st-effect-12" id="menu-12">
 	<h2 class="icon icon-stack">菜单导航</h2>
 	<ul>
-		<li><a class="icon icon-data" href="#">毕业设计需求</a></li>
+        <li><a class="icon icon-data" href="./module_graduation_requirement.php">毕业设计需求</a></li>
 		<li><a class="icon icon-location" href="#">毕业设计文档</a></li>
 		<li><a class="icon icon-study" href="#">毕业设计答辩</a></li>
 		<li><a class="icon icon-photo" href="#">。。。</a></li>
