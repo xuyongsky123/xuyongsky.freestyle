7a35b78cb8d23bdf62a18fa76fc67f8e49a3111a
xuyongsky123 <xuyongsky123@163.com>
2015-03-11 03:10:53 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_blog_templetfooter.php

---
+++ module_blog_templetfooter.php
@@ -0,0 +1,11 @@
+<footer>
+	<div>@Copyright|About us</div>
+      	<ul class="tt-wrapper">
+			<li><a class="tt-gplus" href="#"><span>Google Plus</span></a></li>
+			<li><a class="tt-twitter" href="#"><span>Twitter</span></a></li>
+			<li><a class="tt-dribbble" href="#"><span>Dribbble</span></a></li>
+			<li><a class="tt-facebook" href="#"><span>Facebook</span></a></li>
+			<li><a class="tt-linkedin" href="#"><span>LinkedIn</span></a></li>
+			<li><a class="tt-forrst" href="#"><span>Forrst</span></a></li>
+		</ul>
+</footer>
