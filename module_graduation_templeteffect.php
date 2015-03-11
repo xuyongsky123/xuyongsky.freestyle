29614ef96db2bd7df7ff5d136553a10b85bcefff
xuyongsky123 <xuyongsky123@163.com>
2015-03-11 06:15:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_templeteffect.php

--- module_graduation_templeteffect.php
+++ module_graduation_templeteffect.php
@@ -1,2 +1,2 @@
-<script src="./module_graduation_js/classie.js"></script>
-<script src="./module_graduation_js/sidebarEffects.js"></script>
+<script src="./js/module_graduation_js/classie.js"></script>
+<script src="./js/module_graduation_js/sidebarEffects.js"></script>
