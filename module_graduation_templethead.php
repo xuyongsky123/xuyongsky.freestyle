5af378a23bb5402cd41189cfe7f2d0010d7806ae
xuyongsky123 <xuyongsky123@163.com>
2015-03-11 06:14:44 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_templethead.php

--- module_graduation_templethead.php
+++ module_graduation_templethead.php
@@ -5,7 +5,7 @@
 <meta name="description" content="graduation project" />
 <meta name="keywords" content="graduation project,graduation" />
 <!--<meta name="author" content="Codrops" />-->
-<link rel="stylesheet" type="text/css" href="./module_graduation_css/normalize.css" />
-<link rel="stylesheet" type="text/css" href="./module_graduation_css/demo.css" />
-<link rel="stylesheet" type="text/css" href="./module_graduation_css/component.css" />
+<link rel="stylesheet" type="text/css" href="./css/module_graduation_css/normalize.css" />
+<link rel="stylesheet" type="text/css" href="./css/odule_graduation_css/demo.css" />
+<link rel="stylesheet" type="text/css" href="./css/le_graduation_css/component.css" />
 <script src="./module_graduation_js/modernizr.custom.js"></script>
