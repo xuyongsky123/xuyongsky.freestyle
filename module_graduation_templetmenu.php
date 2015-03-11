0f590b3d1314aabb960fbf0e649839bb3db4b9a2
xuyongsky123 <xuyongsky123@163.com>
2015-03-11 10:10:20 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_templetmenu.php

--- module_graduation_templetmenu.php
+++ module_graduation_templetmenu.php
@@ -3,7 +3,7 @@
 	<ul>
         <li><a class="icon icon-data" href="./module_graduation_requirement.php">毕业设计需求</a></li>
         <li><a class="icon icon-location" href="./module_graduation_document.php">毕业设计文档</a></li>
-		<li><a class="icon icon-study" href="#">毕业设计答辩</a></li>
+        <li><a class="icon icon-study" href="./module_graduation_reply.php">毕业设计答辩</a></li>
 		<li><a class="icon icon-photo" href="#">。。。</a></li>
 	</ul>
 </nav>
