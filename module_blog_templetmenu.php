d56c3f61a5163f5a656692ea9dd61d6ba468a6e7
xuyongsky123 <xuyongsky123@163.com>
2015-03-12 06:51:28 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_blog_templetmenu.php

--- module_blog_templetmenu.php
+++ module_blog_templetmenu.php
@@ -1,5 +1,5 @@
 <nav>
 	<ul>
-		<li><a href="./index.php">Home</a></li><li><a href="./module_blog_writeblog.php">Post</a></li><li><a href="#">Contact</a></li><li><a href="./module_blog_aboutus.php">About</a></li><li><a href="./index.php">Primary-Home</a></li>
+		<li><a href="./module_blog_blog.php">Home</a></li><li><a href="./module_blog_writeblog.php">Post</a></li><li><a href="#">Contact</a></li><li><a href="./module_blog_aboutus.php">About</a></li><li><a href="./index.php">Primary-Home</a></li>
 	</ul>
 </nav>
