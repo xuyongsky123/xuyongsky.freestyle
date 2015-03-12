e2e83274639058db8591b884e3f31b2b6e0c31e8
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-12 07:43:23 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_blog_showarticle.php

--- 3/module_blog_showarticle.php
+++ 3/module_blog_showarticle.php
@@ -19,7 +19,15 @@
 		 	
 	}else{
 		$public_sql="select * from public_article";
-        $array=$operatedb->Execsql($public_sql,$conn);
+        $public_array=$operatedb->Execsql($public_sql,$conn);
+        
+        if($public_array){
+            foreach($public_array as $sub_item){
+            	echo "<li><a href='./module_blog_blog.php?public_filename=".$sub_item['public_filename']."'>".$sub_item['public_filename']."</a></li>"
+            }
+        }else{
+        	echo "数据库暂时未存储blog文章，敬请期待..."
+        }
 	}	
 	
 ?>
