15a27cda429fd666da3f189c045f6dd91540b8d2
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-12 06:21:35 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- 3/module_shop_shop.php
+++ 3/module_shop_shop.php
@@ -1,4 +1,4 @@
 <?php
 	header("Content-Type:text/html;charset=utf-8;");
-	echo "<div style='background:lightgreen;font-size:2rem;margin-top:15%;padding:50px;text-align:center;'>大学网上商城模块，针对大学校园内的电商，功能开发中...</div>"
+	echo "<div style='background:lightgreen;font-size:2rem;margin-top:15%;padding:50px;text-align:center;'>大学网上商城模块，针对大学校园内的电商，功能开发中...</div>";
 ?>
