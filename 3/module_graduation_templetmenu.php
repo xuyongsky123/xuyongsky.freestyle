c3aef9efa6b9216e3b7cf9af6832e44a35a035d0
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-12 07:06:09 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_templetmenu.php

--- 3/module_graduation_templetmenu.php
+++ 3/module_graduation_templetmenu.php
@@ -4,6 +4,6 @@
         <li><a class="icon icon-data" href="./module_graduation_requirement.php">毕业设计需求</a></li>
         <li><a class="icon icon-location" href="./module_graduation_document.php">毕业设计文档</a></li>
         <li><a class="icon icon-study" href="./module_graduation_reply.php">毕业设计答辩</a></li>
-        <li><a class="icon icon-photo" href="http://www.hkjgy.com">黑科大信管毕业答辩系统(友情链接)</a></li>
+        <li><a class="icon icon-photo" href="http://www.hkjgy.com">黑科技管院毕业答辩系统(友情链接)</a></li>
 	</ul>
 </nav>
