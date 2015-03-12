e66b7148f3455707337d2e84122826a60aec2966
xuyongsky123 <xuyongsky123@163.com>
2015-03-12 06:10:49 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_templethead.php

--- module_graduation_templethead.php
+++ module_graduation_templethead.php
@@ -2,8 +2,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 <title>毕业设计专栏</title>
-<meta name="description" content="graduation project" />
-<meta name="keywords" content="graduation project,graduation" />
+<meta name="keywords" content="自由风,dream,sinaapp.com,SAE,大学生电商,大学淘宝店,大学京东店,graduation project,graduation" />
+<meta name="description" content="自由风,freestyle,徐勇个人开发网站,xuyongsky官方网站,大学生电商,大学淘宝店,大学京东店,graduation project">
+<meta name="description" content="" />
+<meta name="keywords" content="" />
 <!--<meta name="author" content="Codrops" />-->
 <link rel="stylesheet" type="text/css" href="./css/module_graduation_css/normalize.css" />
 <link rel="stylesheet" type="text/css" href="./css/module_graduation_css/demo.css" />
