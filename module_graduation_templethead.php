28bff22aecc0d31550ba6a2153c490b395ff3b21
xuyongsky123 <xuyongsky123@163.com>
2015-03-12 01:53:32 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_graduation_templethead.php

--- module_graduation_templethead.php
+++ module_graduation_templethead.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8" />
-<meta http-equiv="X-UA-Compatible" > 
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 <title>毕业设计专栏</title>
 <meta name="description" content="graduation project" />
