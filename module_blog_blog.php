bbe251cae8b168bf026db7505f92e894174f7678
xuyongsky123 <xuyongsky123@163.com>
2015-03-12 13:34:22 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_blog_blog.php

--- module_blog_blog.php
+++ module_blog_blog.php
@@ -72,7 +72,9 @@
                         }else if(isset($_GET['public_filename'])){
                         	$public_filename=$GET['public_filename'];
                             
+                            echo '<article>';
                             echo $public_filename;
+							echo '</article>';
                             
                             $public_sql="select * from public_article where public_filename = '$public_filename'";
                             $public_array=$operatedb->Execsql($public_sql,$conn);
