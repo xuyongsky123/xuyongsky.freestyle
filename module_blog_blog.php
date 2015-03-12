994cf9cb29e9a9a2033e78c3b97ecbd258648016
xuyongsky123 <xuyongsky123@163.com>
2015-03-12 06:11:28 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_blog_blog.php

--- module_blog_blog.php
+++ module_blog_blog.php
@@ -8,7 +8,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
         <meta name="keywords" content="自由风,dream,sinaapp.com,SAE,大学生电商,大学淘宝店,大学京东店" />
     	<meta name="description" content="自由风,freestyle,徐勇个人开发网站,xuyongsky官方网站,大学生电商,大学淘宝店,大学京东店">
-		<title>myblog网站主页</title>
+		<title>博客文章专栏</title>
         <link rel="stylesheet" href="./css/module_blog_style.css" />
 	</head>
 	<body>
