1fb634ddbfc75eec4d67c2abec9b1b226d463419
xuyongsky123 <xuyongsky123@163.com>
2015-03-13 07:55:56 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- module_shop_shop.php
+++ module_shop_shop.php
@@ -1,6 +1,6 @@
 <?php
 	header("Content-Type:text/html;charset=utf-8;");
-	echo '<div style="z-index:100;background:lightgreen;font-size:2rem;padding:50px;text-align:center;">大学网上商城模块，针对大学校园内的电商，功能开发中...</div>';
+echo '<div style="z-index:100;background:lightgreen;margin-top:15%;font-size:2rem;padding:50px;text-align:center;">大学网上商城模块，针对大学校园内的电商，功能开发中...</div>';
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
