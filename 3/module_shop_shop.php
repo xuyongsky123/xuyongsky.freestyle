058823f007d4bf7775f4ba34282cf9b520842ca0
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-13 08:44:15 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- 3/module_shop_shop.php
+++ 3/module_shop_shop.php
@@ -6,7 +6,7 @@ echo '<div style="z-index:100;background:lightgreen;margin-top:15%;font-size:2re
 <html lang="zh-CN">
   <head> 
     <?php include('./frame_bootstrap_templethead.php'); ?>
-      <link href="./css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
+      <link href="./css/module_shop_css/module_shop_style.css" rel="stylesheet" />
 	<title>大学电商专栏</title>
   </head>
   <body>
