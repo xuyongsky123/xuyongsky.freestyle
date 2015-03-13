1d998c1d7e6069b00e0f3197fba6929f3022eabf
xuyongsky123 <xuyongsky123@163.com>
2015-03-13 08:31:59 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- module_shop_shop.php
+++ module_shop_shop.php
@@ -15,7 +15,6 @@ echo '<div style="z-index:100;background:lightgreen;margin-top:15%;font-size:2re
             </div>
             <div class="collapse navbar-collapse" id="navbar-collapse-01">
               <ul class="nav navbar-nav navbar-left">
-                <li><a href="#fakelink">Menu Item<span class="navbar-unread">1</span></a></li>
                 <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Messages <b class="caret"></b></a>
                   <span class="dropdown-arrow"></span>
