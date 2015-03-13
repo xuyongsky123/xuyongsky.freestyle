cdaa5acd7e66e798705e6c0f75158c93c5729f0b
xuyongsky123 <xuyongsky123@163.com>
2015-03-13 04:02:59 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_blog_blog.php

--- module_blog_blog.php
+++ module_blog_blog.php
@@ -26,83 +26,7 @@
 				<div class="clearfix"></div>
 			<div>
 		</header>
-		<?php include("./module_blog_templetmenu.php"); ?>
-		<section>
-			<aside>
-				<article>
-                    <div id="upload" style="display:none;">
-						<em>在此上传您的blog文章：</em>
-						<form enctype="multipart/form-data" name="upload_form" method="post" action="module_blog_handleupload.php">
-							<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
-							<input type="file" name="upload_file" />
-							<input type="submit" value="上传文件" />
-						</form>
-					</div>
-					<h3>文章列表：</h3>
-					<div id="article">
-						<ul>
-							<?php require("./module_blog_showarticle.php"); ?>
-						</ul>
-					</div>
-				</article>	
-			</aside>
-			<div id="content">
-					<?php 
-						if(isset($_GET['filename'])){
-		
-							//这里的$_SESSION['username']并不是没有意义，而是唯一标识某个用户的文章，区别于其他用户的同名文章
-							//session_start();
-							
-							$user_search=$_SESSION['username'];
-							$file_search=$_GET['filename'];
-							$sql_search="select * from user_article where filename = '$file_search' and username='$user_search' ";
-							$array_search=$operatedb->Execsql($sql_search,$conn);
-                            
-                            if($array_search){
-                            	foreach($array_search as $item_search){
-                                    echo '<article>';
-                                    echo $item_search['article'];	
-                                    echo '</article>';
-                                }
-                            }else{
-                            	echo '<article>';
-                                echo "查询数据库中文章时出错，我们会尽快处理，请谅解...";	
-                                echo '</article>';
-                            }
-							
-                        }else if(isset($_GET['public_filename'])){
-                        	$public_filename=$_GET['public_filename'];
-                            $public_sql="select * from public_article where public_filename = '$public_filename'";
-                            $public_array=$operatedb->Execsql($public_sql,$conn);
-
-                            
-                            if($public_array){
-                                foreach($public_array as $item_public){
-                                	echo '<article>';
-                                    echo $item_public['public_article'];
-                                    echo '</article>';
-                                }
-                            }else{
-                            	echo '<article>';
-                                echo "查询数据库中文章时出错，我们会尽快处理，请谅解...";	
-                                echo '</article>';
-                            }
-                        }else{
-							echo '<article>';
-							echo "公共文章，不属于具体某人所有...";
-							echo '</article>';
-						}
-					?>
-			</div>
-		</section>
-                
-        <!--footer-->
-           <?php include("./module_blog_templetfooter.php"); ?>
-                
-		<div id="top">
-			<a href="javascript:window.scrollTo(0,0);" >回到顶部</a>
-		</div>
-        <!--HTML5背景动态云效果-Begin-->
+                <!--HTML5背景动态云效果-Begin-->
     <script type="text/javascript" src="./js/webgl_clouds_js/three.min.js"></script>
 		<script type="text/javascript" src="./js/webgl_clouds_js/Detector.js"></script>
 		<script id="vs" type="x-shader/x-vertex">
@@ -275,5 +199,82 @@
 
 		</script>
     <!--HTML5背景动态云效果-End-->
+		<?php include("./module_blog_templetmenu.php"); ?>
+		<section>
+			<aside>
+				<article>
+                    <div id="upload" style="display:none;">
+						<em>在此上传您的blog文章：</em>
+						<form enctype="multipart/form-data" name="upload_form" method="post" action="module_blog_handleupload.php">
+							<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+							<input type="file" name="upload_file" />
+							<input type="submit" value="上传文件" />
+						</form>
+					</div>
+					<h3>文章列表：</h3>
+					<div id="article">
+						<ul>
+							<?php require("./module_blog_showarticle.php"); ?>
+						</ul>
+					</div>
+				</article>	
+			</aside>
+			<div id="content">
+					<?php 
+						if(isset($_GET['filename'])){
+		
+							//这里的$_SESSION['username']并不是没有意义，而是唯一标识某个用户的文章，区别于其他用户的同名文章
+							//session_start();
+							
+							$user_search=$_SESSION['username'];
+							$file_search=$_GET['filename'];
+							$sql_search="select * from user_article where filename = '$file_search' and username='$user_search' ";
+							$array_search=$operatedb->Execsql($sql_search,$conn);
+                            
+                            if($array_search){
+                            	foreach($array_search as $item_search){
+                                    echo '<article>';
+                                    echo $item_search['article'];	
+                                    echo '</article>';
+                                }
+                            }else{
+                            	echo '<article>';
+                                echo "查询数据库中文章时出错，我们会尽快处理，请谅解...";	
+                                echo '</article>';
+                            }
+							
+                        }else if(isset($_GET['public_filename'])){
+                        	$public_filename=$_GET['public_filename'];
+                            $public_sql="select * from public_article where public_filename = '$public_filename'";
+                            $public_array=$operatedb->Execsql($public_sql,$conn);
+
+                            
+                            if($public_array){
+                                foreach($public_array as $item_public){
+                                	echo '<article>';
+                                    echo $item_public['public_article'];
+                                    echo '</article>';
+                                }
+                            }else{
+                            	echo '<article>';
+                                echo "查询数据库中文章时出错，我们会尽快处理，请谅解...";	
+                                echo '</article>';
+                            }
+                        }else{
+							echo '<article>';
+							echo "公共文章，不属于具体某人所有...";
+							echo '</article>';
+						}
+					?>
+			</div>
+		</section>
+                
+        <!--footer-->
+           <?php include("./module_blog_templetfooter.php"); ?>
+                
+		<div id="top">
+			<a href="javascript:window.scrollTo(0,0);" >回到顶部</a>
+		</div>
+        
 	</body>
 </html>
