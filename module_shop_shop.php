2a1598f7d017e4a7c378fc94d5f9d14d514c0c15
xuyongsky123 <xuyongsky123@163.com>
2015-03-13 10:53:20 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- module_shop_shop.php
+++ module_shop_shop.php
@@ -72,5 +72,60 @@
               </form>
             </div><!-- /.navbar-collapse -->
           </nav><!-- /navbar -->
+      
+      <div class="row demo-samples">
+        <div class="col-xs-4">
+          <div class="todo">
+            <div class="todo-search">
+              <input class="todo-search-field" type="search" value="" placeholder="Search" />
+            </div>
+            <ul>
+              <li class="todo-done">
+                <div class="todo-icon fui-user"></div>
+                <div class="todo-content">
+                  <h4 class="todo-name">
+                    Meet <strong>Adrian</strong> at <strong>6pm</strong>
+                  </h4>
+                  Times Square
+                </div>
+              </li>
+              <li>
+                <div class="todo-icon fui-list"></div>
+                <div class="todo-content">
+                  <h4 class="todo-name">
+                    Chat with <strong>V.Kudinov</strong>
+                  </h4>
+                  Skype conference an 9 am
+                </div>
+              </li>
+              <li>
+                <div class="todo-icon fui-eye"></div>
+                <div class="todo-content">
+                  <h4 class="todo-name">
+                    Watch <strong>Iron Man</strong>
+                  </h4>
+                  1998 Broadway
+                </div>
+              </li>
+              <li>
+                <div class="todo-icon fui-time"></div>
+                <div class="todo-content">
+                  <h4 class="todo-name">
+                    Fix bug on a <strong>Website</strong>
+                  </h4>
+                  As soon as possible
+                </div>
+              </li>
+            </ul>
+          </div><!-- /.todo -->
+        </div><!-- /.col-xs-4 -->
+
+        <div class="col-xs-8">
+          <video class="video-js" preload="auto" poster="docs/assets/img/video/poster.jpg" data-setup="{}">
+            <source src="http://iurevych.github.com/Flat-UI-videos/big_buck_bunny.mp4" type="video/mp4">
+            <source src="http://iurevych.github.com/Flat-UI-videos/big_buck_bunny.webm" type="video/webm">
+          </video>
+        </div> <!-- /video -->
+      </div>
   </body>
 </html>
