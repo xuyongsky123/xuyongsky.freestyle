140be8149444689d5407eb26c522e3f9c0c79540
xuyongsky123 <xuyongsky123@163.com>
2015-03-13 07:47:11 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_templethead.php

--- module_shop_templethead.php
+++ module_shop_templethead.php
@@ -5,6 +5,6 @@
 <!-- Bootstrap -->
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="http://cdn.bootcss.com/jquery/1.11.2/jquery.min.js"></script>
+<script src="./js/jquery-1.9.1.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
