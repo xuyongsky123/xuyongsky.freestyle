23acddc8eb23663de88a8a563334b9492fe52975
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-13 10:29:43 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- 3/module_shop_shop.php
+++ 3/module_shop_shop.php
@@ -15,6 +15,7 @@
           <div id="link_help">
               <span><a>邀请好友</a></span>
               <span><a>加入收藏</a></span>
+              <span><a>关注“自由风”</a></span>
           </div>
       	<div id="shop_header">
 			<div id="login">
