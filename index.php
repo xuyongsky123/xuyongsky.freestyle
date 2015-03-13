079cf879e4686cf0553115ca7ba1baf50cd225cf
xuyongsky123 <xuyongsky123@163.com>
2015-03-13 03:42:22 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/index.php

--- index.php
+++ index.php
@@ -228,7 +228,7 @@
 
 				geometry = new THREE.Geometry();
 
-				var texture = THREE.ImageUtils.loadTexture( 'cloud10.png', null, animate );
+                var texture = THREE.ImageUtils.loadTexture( './images/home_bg/cloud10.png', null, animate );
 				texture.magFilter = THREE.LinearMipMapLinearFilter;
 				texture.minFilter = THREE.LinearMipMapLinearFilter;
 
