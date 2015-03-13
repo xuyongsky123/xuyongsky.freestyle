2ad94dbf451fa1ac69dadcc98537c2e6467d5c40
xuyongsky123 <xuyongsky123@163.com>
2015-03-13 08:41:40 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/frame_bootstrap_templethead.php

--- frame_bootstrap_templethead.php
+++ frame_bootstrap_templethead.php
@@ -1,8 +1,8 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <!-- Bootstrap -->
-<link href="./css/bootstrap_css/bootstrap.min.css" rel="stylesheet">
+<link href="./css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="./js/jquery-1.9.1.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -10,5 +10,5 @@
 
 
 <!-- Loading Flat UI -->
-<link href="./css/bootstrap_css/flat_ui_css/flat-ui.css" rel="stylesheet">
-<link href="./css/bootstrap_css/flat_ui_css/demo.css" rel="stylesheet">
+<link href="./css/bootstrap_css/flat_ui_css/flat-ui.css" rel="stylesheet" />
+<link href="./css/bootstrap_css/flat_ui_css/demo.css" rel="stylesheet" />
