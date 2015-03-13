eeef684641103dfc4ffe667861d560e9db8b21b4
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-13 07:57:15 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- 3/module_shop_shop.php
+++ 3/module_shop_shop.php
@@ -5,10 +5,19 @@ echo '<div style="z-index:100;background:lightgreen;margin-top:15%;font-size:2re
 <!DOCTYPE html>
 <html lang="zh-CN">
     <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">
+        
     </head>
     <body>
         
     </body>
 </html>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>大学电商专栏</title>
+<!-- Bootstrap -->
+<link href="./css/bootstrap_css/bootstrap.min.css" rel="stylesheet">
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="./js/jquery-1.9.1.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="./js/bootstrap_js/bootstrap.min.js"></script>
