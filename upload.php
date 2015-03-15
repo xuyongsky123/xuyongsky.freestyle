aa9f096c34dd5f7ab55d69370563ca78a65bbe4f
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 15:16:17 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/upload.php

--- upload.php
+++ upload.php
@@ -1,25 +1,4 @@
-<?php 
-	session_start();
-	header("Content-Type:text/html;charset=utf-8");
-if(isset($_POST['upload_file'])){
-			$data=$_POST['upload_file'];
-			$PSize = filesize($data); 
-			$article = addslashes(fread(fopen($data, "r"), $PSize)); 
-			require("./database/connDB.inc.php");
-			
-            $sql="insert into shop_single_detail(shop_name,shop_keeper,goods_name,goods_price,goods_detail,goods_image,goods_style_top,goods_style_bottom) values('品牌手机小店','闭上眼 会有好梦吗','iPad','待商定价','iPad','$article','palette-sun-flower','palette-orange')";
-            //$sql="insert into public_article(public_filename,public_article,public_author) values('$blog_title','$data','$user')";
-			$result=$operatedb->Execsql($sql,$conn);
-			
-			if($result){
-				echo "<script language='javascript'>alert('保存编辑到数据库成功');history.back();</script>";
-			}else{
-				echo "<script language='javascript'>alert('保存编辑到数据库失败');history.back();</script>";	
-			}
-}else{
-	echo "<script language='javascript'>alert('没有数据');history.back();</script>";
-}
-?>
+
 <?php
  /**
  * 向数据库中插入图片
