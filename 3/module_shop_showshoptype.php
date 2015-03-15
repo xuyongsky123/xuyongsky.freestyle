86f2322354fced4dd7f3f54644b0d20c2a5c56f8
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-15 08:20:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_showshoptype.php

--- 3/module_shop_showshoptype.php
+++ 3/module_shop_showshoptype.php
@@ -10,8 +10,8 @@
 			echo '<li>';
 			echo '<div class="fix_style">';
 			echo '<h4 class="todo-name">';
-			echo '<a href="./module_blog_blog.php?filename='.$item["filename"].'">';
-			echo $item["filename"];
+			echo '<a href="./module_blog_blog.php?filename='.$item["shop_type"].'">';
+			echo $item["shop_type"];
 			echo '</a>';
 			echo '</h4>';
 			echo '</div>';
