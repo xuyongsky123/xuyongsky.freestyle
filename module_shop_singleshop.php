d67d8b86a5855d7f534e666595c9d0cf008b3aab
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 13:57:59 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_singleshop.php

--- module_shop_singleshop.php
+++ module_shop_singleshop.php
@@ -34,6 +34,7 @@
                                             foreach($array_search as $item_search){
                                                 echo '<div class="pallete-item xuyong_fixitem">';
                                                 echo '<dl class="palette palette-turquoise">';
+                                                echo '<img src="" />';
                                                 echo '</dl>';
                                                 echo '<dt>';
                                                 //echo '<a href="./module_shop_singleshop.php?shop_name='.$item_search["goods_name"].'&shop_keeper='.$item_search["shop_keeper"].'">';
@@ -68,94 +69,103 @@
                                 
                                 
                                 
-                          <div class="pallete-item xuyong_fixitem">
+                          <div class="pallete-item">
                               <dl class="palette palette-turquoise">
-                                <dt>#1abc9c</dt>
-                                <dd>Turquoise</dd>
+                                  <img src="" />
                               </dl>
                               <dl class="palette palette-green-sea">
-                                  <img src="./images/1.jpg"/>
+                                <dt>#16a085</dt>
+                                <dd>Green sea</dd>
                               </dl>
                           </div>
-                          <div class="pallete-item xuyong_fixitem">
+                          <div class="pallete-item">
                               <dl class="palette palette-emerald">
                                   <dt>#2ecc71</dt>
                                   <dd>Emerald</dd>
                                 </dl>
                                 <dl class="palette palette-nephritis">
-                                  <img src="./images/1.jpg"/>
+                                  <dt>#27ae60</dt>
+                                  <dd>Nephritis</dd>
                                 </dl>
                           </div>
-                          <div class="pallete-item xuyong_fixitem">
+                          <div class="pallete-item">
                               <dl class="palette palette-peter-river">
                                   <dt>#3498db</dt>
                                   <dd>Peter river</dd>
                                 </dl>
                                 <dl class="palette palette-belize-hole">
-                                  <img src="./images/1.jpg"/>
+                                  <dt>#2980b9</dt>
+                                  <dd>Belize hole</dd>
                                 </dl>
                           </div>
-                          <div class="pallete-item xuyong_fixitem">
+                          <div class="pallete-item">
                               <dl class="palette palette-amethyst">
                                   <dt>#9b59b6</dt>
                                   <dd>Amethyst</dd>
                                 </dl>
                                 <dl class="palette palette-wisteria">
-                                  <img src="./images/1.jpg"/>
+                                  <dt>#8e44ad</dt>
+                                  <dd>Wisteria</dd>
                                 </dl>
                           </div>
-                          <div class="pallete-item xuyong_fixitem">
+                          <div class="pallete-item">
                               <dl class="palette palette-wet-asphalt">
                                   <dt>#34495e</dt>
                                   <dd>Wet asphalt</dd>
                                 </dl>
                                 <dl class="palette palette-midnight-blue">
-                                  <img src="./images/1.jpg"/>
+                                  <dt>#2c3e50</dt>
+                                  <dd>Midnight blue</dd>
                                 </dl>
                           </div>
-                          <div class="pallete-item xuyong_fixitem">
+                          <div class="pallete-item">
                               <dl class="palette palette-sun-flower">
                                   <dt>#f1c40f</dt>
                                   <dd>Sun flower</dd>
                                 </dl>
                                 <dl class="palette palette-orange">
-                                  <img src="./images/1.jpg"/>
+                                  <dt>#f39c12</dt>
+                                  <dd>Orange</dd>
                                 </dl>
                           </div>
-                          <div class="pallete-item xuyong_fixitem">
+                          <div class="pallete-item">
                               <dl class="palette palette-carrot">
                                   <dt>#e67e22</dt>
                                   <dd>Carrot</dd>
                                 </dl>
                                 <dl class="palette palette-pumpkin">
-                                  <img src="./images/1.jpg"/>
+                                  <dt>#d35400</dt>
+                                  <dd>Pumpkin</dd>
                                 </dl>
                           </div>
-                          <div class="pallete-item xuyong_fixitem">
+                          <div class="pallete-item">
                               <dl class="palette palette-alizarin">
                                   <dt>#e74c3c</dt>
                                   <dd>Alizarin</dd>
                                 </dl>
                                 <dl class="palette palette-pomegranate">
-                                  <img src="./images/1.jpg"/>
+                                  <dt>#c0392b</dt>
+                                  <dd>Pomegranate</dd>
                                 </dl>
                           </div>
-                          <div class="pallete-item xuyong_fixitem">
+                          <div class="pallete-item">
                               <dl class="palette palette-clouds">
                                   <dt>#ecf0f1</dt>
                                   <dd>Clouds</dd>
                                 </dl>
                                 <dl class="palette palette-silver">
-                                  <img src="./images/1.jpg"/>
+                                  <dt>#bdc3c7</dt>
+                                  <dd>Silver</dd>
                                 </dl>
                           </div>
-                          <div class="pallete-item xuyong_fixitem">
+                          <div class="pallete-item">
                               <dl class="palette palette-concrete">
                                   <dt>#95a5a6</dt>
                                   <dd>Concrete</dd>
                                 </dl>
                                 <dl class="palette palette-asbestos">
-                                  <img src="./images/1.jpg"/>
+                                  <dt>#7f8c8d</dt>
+                                  <dd>Asbestos</dd>
                                 </dl>
                           </div>
                                 
