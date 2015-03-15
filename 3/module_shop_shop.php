f96a839e003b51f4dfcb8d84cae0788a5d62516c
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-15 09:15:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- 3/module_shop_shop.php
+++ 3/module_shop_shop.php
@@ -90,149 +90,49 @@
             	
                 	<div class="swatches-col">
                         <?php 
-						if(isset($_GET['shop_type'])){
-                            
-							$file_search=$_GET['shop_type'];
-							$sql_search="select * from shop_info where shop_type = '$file_search'";
-							$array_search=$operatedb->Execsql($sql_search,$conn);
-                            
-                            if($array_search){
-                            	foreach($array_search as $item_search){
-                                    echo '<div class="pallete-item">';
-                                    echo '<dl class="palette '.$item_search["shop_style_top"].'">';
-                                    echo '<dt>';
-                                    echo '<a>';
-                                    echo $item_search["shop_name"];
-                                    echo '</a>';
-                                    echo '</dt>';
-                                    echo '<dd>';
-                                    echo $item_search["shop_keeper"];
-                                    echo '</dd>';
-                                    echo '</dl>';
-                                    echo '<dl class="palette '.$item_search["shop_style_bottom"].'">';
-                                    echo '<dt>';
-                                    echo '<a>';
-                                    echo $item_search["shop_name"];
-                                    echo '</a>';
-                                    echo '</dt>';
-                                    echo '<dd>';
-                                    echo $item_search["shop_keeper"];
-                                    echo '</dd>';
-                                    echo '</dl>';
+                            if(isset($_GET['shop_type'])){
+                                
+                                $file_search=$_GET['shop_type'];
+                                $sql_search="select * from shop_info where shop_type = '$file_search'";
+                                $array_search=$operatedb->Execsql($sql_search,$conn);
+                                
+                                if($array_search){
+                                    foreach($array_search as $item_search){
+                                        echo '<div class="pallete-item">';
+                                        echo '<dl class="palette '.$item_search["shop_style_top"].'">';
+                                        echo '<dt>';
+                                        echo '<a>';
+                                        echo $item_search["shop_name"];
+                                        echo '</a>';
+                                        echo '</dt>';
+                                        echo '<dd>';
+                                        echo $item_search["shop_keeper"];
+                                        echo '</dd>';
+                                        echo '</dl>';
+                                        echo '<dl class="palette '.$item_search["shop_style_bottom"].'">';
+                                        echo '<dt>';
+                                        echo '<a>';
+                                        echo $item_search["shop_name"];
+                                        echo '</a>';
+                                        echo '</dt>';
+                                        echo '<dd>';
+                                        echo $item_search["shop_keeper"];
+                                        echo '</dd>';
+                                        echo '</dl>';
+                                        echo '</div>';
+                                    }
+                                }else{
+                                    echo '<div>';
+                                    echo "对不起，您所选择的商店种类暂时没有，请选择其他种类商店...";	
                                     echo '</div>';
                                 }
+                                
                             }else{
-                            	echo '<div>';
-                                echo "对不起，您所选择的商店种类暂时没有，请选择其他种类商店...";	
+                                echo '<div>';
+                                echo "您未选择任何商店种类，请从左边列表选择...";
                                 echo '</div>';
                             }
-							
-                        }else{
-							echo '<div>';
-							echo "您未选择任何商店种类，请从左边列表选择...";
-							echo '</div>';
-						}
-					?>
-                          <div class="pallete-item">
-                              <dl class="palette palette-turquoise">
-                                <dt>#1abc9c</dt>
-                                <dd>Turquoise</dd>
-                              </dl>
-                              <dl class="palette palette-green-sea">
-                                <dt>#16a085</dt>
-                                <dd>Green sea</dd>
-                              </dl>
-                          </div>
-                          <div class="pallete-item">
-                              <dl class="palette palette-emerald">
-                                  <dt>#2ecc71</dt>
-                                  <dd>Emerald</dd>
-                                </dl>
-                                <dl class="palette palette-nephritis">
-                                  <dt>#27ae60</dt>
-                                  <dd>Nephritis</dd>
-                                </dl>
-                          </div>
-                          <div class="pallete-item">
-                              <dl class="palette palette-peter-river">
-                                  <dt>#3498db</dt>
-                                  <dd>Peter river</dd>
-                                </dl>
-                                <dl class="palette palette-belize-hole">
-                                  <dt>#2980b9</dt>
-                                  <dd>Belize hole</dd>
-                                </dl>
-                          </div>
-                          <div class="pallete-item">
-                              <dl class="palette palette-amethyst">
-                                  <dt>#9b59b6</dt>
-                                  <dd>Amethyst</dd>
-                                </dl>
-                                <dl class="palette palette-wisteria">
-                                  <dt>#8e44ad</dt>
-                                  <dd>Wisteria</dd>
-                                </dl>
-                          </div>
-                          <div class="pallete-item">
-                              <dl class="palette palette-wet-asphalt">
-                                  <dt>#34495e</dt>
-                                  <dd>Wet asphalt</dd>
-                                </dl>
-                                <dl class="palette palette-midnight-blue">
-                                  <dt>#2c3e50</dt>
-                                  <dd>Midnight blue</dd>
-                                </dl>
-                          </div>
-                          <div class="pallete-item">
-                              <dl class="palette palette-sun-flower">
-                                  <dt>#f1c40f</dt>
-                                  <dd>Sun flower</dd>
-                                </dl>
-                                <dl class="palette palette-orange">
-                                  <dt>#f39c12</dt>
-                                  <dd>Orange</dd>
-                                </dl>
-                          </div>
-                          <div class="pallete-item">
-                              <dl class="palette palette-carrot">
-                                  <dt>#e67e22</dt>
-                                  <dd>Carrot</dd>
-                                </dl>
-                                <dl class="palette palette-pumpkin">
-                                  <dt>#d35400</dt>
-                                  <dd>Pumpkin</dd>
-                                </dl>
-                          </div>
-                          <div class="pallete-item">
-                              <dl class="palette palette-alizarin">
-                                  <dt>#e74c3c</dt>
-                                  <dd>Alizarin</dd>
-                                </dl>
-                                <dl class="palette palette-pomegranate">
-                                  <dt>#c0392b</dt>
-                                  <dd>Pomegranate</dd>
-                                </dl>
-                          </div>
-                          <div class="pallete-item">
-                              <dl class="palette palette-clouds">
-                                  <dt>#ecf0f1</dt>
-                                  <dd>Clouds</dd>
-                                </dl>
-                                <dl class="palette palette-silver">
-                                  <dt>#bdc3c7</dt>
-                                  <dd>Silver</dd>
-                                </dl>
-                          </div>
-                          <div class="pallete-item">
-                              <dl class="palette palette-concrete">
-                                  <dt>#95a5a6</dt>
-                                  <dd>Concrete</dd>
-                                </dl>
-                                <dl class="palette palette-asbestos">
-                                  <dt>#7f8c8d</dt>
-                                  <dd>Asbestos</dd>
-                                </dl>
-                          </div>
+                        ?>
                     </div> <!-- /swatches items -->
                 
             </div>
