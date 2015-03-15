55a95bd55ee2e9c9072de8499ef962be4bac122c
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 12:28:09 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_singleshop.php

--- module_shop_singleshop.php
+++ module_shop_singleshop.php
@@ -21,6 +21,9 @@
             	
                             <div class="swatches-col">
                                 <?php 
+									
+									require("./database/connDB.inc.php");
+
                                     if(isset($_GET['shop_name'])){
                                         
                                         $file_search=$_GET['shop_name'];
