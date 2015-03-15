fc39e5d19e380377077cac817efa1e4cfb25efd9
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 04:44:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- module_shop_shop.php
+++ module_shop_shop.php
@@ -117,10 +117,6 @@
         </div><!-- /.col-xs-4 -->
           
         <div class="col-xs-9">
-          <video class="video-js" preload="auto" poster="docs/assets/img/video/poster.jpg" data-setup="{}">
-            <source src="http://iurevych.github.com/Flat-UI-videos/big_buck_bunny.mp4" type="video/mp4">
-            <source src="http://iurevych.github.com/Flat-UI-videos/big_buck_bunny.webm" type="video/webm">
-          </video>
         </div> <!-- /video -->
       </div>
       
