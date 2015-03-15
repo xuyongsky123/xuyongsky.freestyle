1243e87acf409fd7ffdf454a9172b5b399274700
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 08:11:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_showshoptype.php

--- module_shop_showshoptype.php
+++ module_shop_showshoptype.php
@@ -1,33 +1,24 @@
 <?php
 	require("./database/connDB.inc.php");
-	
-	
-	if(isset($_SESSION['id'])){
 		
-		$user=$_SESSION['username'];
-		$sql="select * from user_article where username = '$user' ";
-		$array=$operatedb->Execsql($sql,$conn);
+	$user=$_SESSION['username'];
+	$sql="select * from shop_info where username = '$user' ";
+	$array=$operatedb->Execsql($sql,$conn);
         
-        if($array){
-        	foreach($array as $item){
-				echo "<li><a href='./module_blog_blog.php?filename=".$item['filename']."'>".$item['filename']."</a></li>";
-			}
-        }else{
-        	echo "暂时还没有文章...";
-        }
-		
-		 	
-	}else{
-		$public_sql="select * from public_article";
-        $public_array=$operatedb->Execsql($public_sql,$conn);
-        
-        if($public_array){
-            foreach($public_array as $sub_item){
-                echo "<li><h4 class='todo-name'><a href='./module_blog_blog.php?public_filename=".$sub_item['public_filename']."'>".$sub_item['public_filename']."</a></h4></li>";
-            }
-        }else{
-        	echo "数据库暂时未存储供欣赏的blog文章，敬请期待...";
-        }
-	}	
+    if($array){
+        foreach($array as $item){
+			echo '<li>';
+			echo '<div class="fix_style">';
+			echo '<h4 class="todo-name">';
+			echo '<a href="./module_blog_blog.php?filename='.$item["filename"].'">';
+			echo $item["filename"];
+			echo '</a>';
+			echo '</h4>';
+			echo '</div>'
+			echo '</li>';
+		}
+    }else{
+        echo "暂时还没有文章...";
+    }
 	
 ?>
