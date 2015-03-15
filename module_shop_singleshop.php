5d706a4584e714b0600864aec022e35df415c9ee
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 12:29:22 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_singleshop.php

--- module_shop_singleshop.php
+++ module_shop_singleshop.php
@@ -34,7 +34,6 @@
                                         if($array_search){
                                             foreach($array_search as $item_search){
                                                 echo '<div class="pallete-item">';
-                                                echo '<dl class="palette '.$item_search["shop_style_top"].'">';
                                                 echo '<dt>';
                                                 //echo '<a href="./module_shop_singleshop.php?shop_name='.$item_search["goods_name"].'&shop_keeper='.$item_search["shop_keeper"].'">';
                                                 echo $item_search["goods_name"];
@@ -43,8 +42,6 @@
                                                 echo '<dd>';
                                                 echo $item_search["goods_price"];
                                                 echo '</dd>';
-                                                echo '</dl>';
-                                                echo '<dl class="palette '.$item_search["shop_style_bottom"].'">';
                                                 echo '<dt>';
                                                 //echo '<a>';
                                                 //echo $item_search["shop_name"];
@@ -53,7 +50,6 @@
                                                 echo '<dd>';
                                                 echo $item_search["goods_detail"];
                                                 echo '</dd>';
-                                                echo '</dl>';
                                                 echo '</div>';
                                             }
                                         }else{
