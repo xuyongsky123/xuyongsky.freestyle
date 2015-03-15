04de8c3bc1571a29913161b1daedbafdbaa9c4e7
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-15 11:03:23 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- 3/module_shop_shop.php
+++ 3/module_shop_shop.php
@@ -9,67 +9,7 @@
 	<title>大学电商专栏</title>
   </head>
   <body>
-      <div id="shop_container">
-          <div id="link_help">
-              <span><a href="#">邀请好友</a></span>
-              <span>|</span>
-              <span><a href="#">加入收藏</a></span>
-              <span>|</span>
-              <span><a href="#">关注“自由风”</a></span>
-          </div>
-            <div id="shop_header">
-                <div id="login">
-                    <?php require("./check_login.php"); ?>
-                    <span>|</span>
-              		<span><a href="#">QQ登录</a></span>
-              		<span>|</span>
-              		<span><a href="#">微博登陆</a></span>
-                </div>
-            </div>
-          <div class="clearfix"></div>
-      </div>
-<header style="height:100px;font-size:5rem;min-width:970px;">
-    <div id="logo" style="text-align:left;padding:7px 0 7px 7px;display:inline-block;">
-        	College Mall
-        </div>
-        
-    </header>
-    <nav class="navbar navbar-inverse navbar-embossed" role="navigation">
-            <div class="collapse navbar-collapse" id="navbar-collapse-01">
-              <ul class="nav navbar-nav navbar-left">
-                <li><a href="./module_shop_shop.php">Home</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Introduce</a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">sub menu one</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">sub menu two</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">sub menu three</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">sub menu four</a></li>
-                  </ul>
-                </li>
-                <li><a href="#fakelink">About Us</a></li>
-                <li><a href="./index.php">Primary-Home</a></li>
-              </ul>
-                
-                
-                
-              <form class="navbar-form navbar-right" action="#" role="search">
-                <div class="form-group">
-                  <div class="input-group">
-                    <input class="form-control" id="navbarInput-01" type="search" placeholder="Search">
-                    <span class="input-group-btn">
-                      <button type="submit" class="btn xuyong_fix">
-                        搜索
-                      </button>
-                    </span>
-                  </div>
-                </div>
-              </form>
-            </div><!-- /.navbar-collapse -->
-          </nav><!-- /navbar -->
+      <?php include("./module_shop_templetheader.php"); ?>
       
       <div class="row demo-samples">
         <div class="col-xs-3">
