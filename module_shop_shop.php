032a0e1af7bf46c1cb130572f1b351833b5a3f19
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 05:12:40 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- module_shop_shop.php
+++ module_shop_shop.php
@@ -76,7 +76,7 @@
       <div class="row demo-samples">
         <div class="col-xs-3">
           <div class="todo">
-            <div>
+            <div class="todo-search">
               <input class="todo-search-field" type="search" value="" placeholder="店铺类型" />
             </div>
             <ul>
