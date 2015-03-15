83e155c6615c405136e1f6757f916421a8ae39b9
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 14:47:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/upload.php

--- upload.php
+++ upload.php
@@ -1,9 +1,19 @@
-<?php header("Content-Type:text/html;charset=utf-8"); ?>
-<div id="upload" >
-						<em>在此上传您的blog文章：</em>
-						<form enctype="multipart/form-data" name="upload_form" method="post" action="module_blog_handleupload.php">
-							<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
-							<input type="file" name="upload_file" />
-							<input type="submit" value="上传文件" />
-						</form>
-					</div>
+<?php 
+	session_start();
+	header("Content-Type:text/html;charset=utf-8");
+
+			$data=$_POST['upload_file'];
+			$article=addslashes($data);
+			
+			require("./database/connDB.inc.php");
+			
+            $sql="insert into shop_single_detail(shop_name,shop_keeper,goods_name,goods_price,goods_detail,goods_image,goods_style_top,goods_style_bottom) values('品牌手机小店','闭上眼 会有好梦吗','iPad','待商定价','iPad','$article','palette-sun-flower','palette-orange')";
+            //$sql="insert into public_article(public_filename,public_article,public_author) values('$blog_title','$data','$user')";
+			$result=$operatedb->Execsql($sql,$conn);
+			
+			if($result){
+				echo "<script language='javascript'>alert('保存编辑到数据库成功');history.back();</script>";
+			}else{
+				echo "<script language='javascript'>alert('保存编辑到数据库失败');history.back();</script>";	
+			}
+?>
