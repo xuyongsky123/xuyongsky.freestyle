35bb536c038d555db3d9c761c2864adf2966e1ea
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 14:20:05 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/upload.php

---
+++ upload.php
@@ -0,0 +1,8 @@
+<div id="upload" style="display:none;">
+						<em>在此上传您的blog文章：</em>
+						<form enctype="multipart/form-data" name="upload_form" method="post" action="module_blog_handleupload.php">
+							<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+							<input type="file" name="upload_file" />
+							<input type="submit" value="上传文件" />
+						</form>
+					</div>
