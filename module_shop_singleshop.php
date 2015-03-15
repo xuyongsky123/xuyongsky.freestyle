c45386099e4ee6c9429047457022242bc5851dff
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 12:22:58 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_singleshop.php

--- module_shop_singleshop.php
+++ module_shop_singleshop.php
@@ -28,33 +28,6 @@
                                         $sql_search="select * from shop_single_detail where shop_name = '$file_search' and shop_keeper='$shop_keeper'";
                                         $array_search=$operatedb->Execsql($sql_search,$conn);
                                         
-                                        if($array_search){
-                                            foreach($array_search as $item_search){
-                                                echo '<div class="pallete-item">';
-                                                echo '<dt>';
-                                                //echo '<a href="./module_shop_singleshop.php?shop_name='.$item_search["goods_name"].'&shop_keeper='.$item_search["shop_keeper"].'">';
-                                                echo $item_search["goods_name"];
-                                                //echo '</a>';
-                                                echo '</dt>';
-                                                echo '<dd>';
-                                                echo $item_search["goods_price"];
-                                                echo '</dd>';
-                                                echo '<dt>';
-                                                //echo '<a>';
-                                                //echo $item_search["shop_name"];
-                                                //echo '</a>';
-                                                echo '</dt>';
-                                                echo '<dd>';
-                                                echo $item_search["goods_detail"];
-                                                echo '</dd>';
-                                                echo '</div>';
-                                            }
-                                        }else{
-                                            echo '<div>';
-                                            echo "对不起，您所选择的商店种类暂时没有，请选择其他种类商店...";	
-                                            echo '</div>';
-                                        }
-                                        
                                     }else{
                                         echo '<div>';
                                         echo "您未选择任何商店种类，请从左边列表选择...";
