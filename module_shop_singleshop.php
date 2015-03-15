aa17e0a98b944a00567c599e88095808ccce1ffc
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 12:06:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_singleshop.php

--- module_shop_singleshop.php
+++ module_shop_singleshop.php
@@ -17,8 +17,58 @@
                 </div><!-- /.col-xs-1-->
                   
                 <div class="col-xs-10 xuyong_fixrow">
-                    
+                    <div class="content_container">
+            	
+                            <div class="swatches-col">
+                                <?php 
+                                    if(isset($_GET['shop_name'])){
+                                        
+                                        $file_search=$_GET['shop_name'];
+                                        $sql_search="select * from shop_info where shop_type = '$file_search'";
+                                        $array_search=$operatedb->Execsql($sql_search,$conn);
+                                        
+                                        if($array_search){
+                                            foreach($array_search as $item_search){
+                                                echo '<div class="pallete-item">';
+                                                echo '<dl class="palette '.$item_search["shop_style_top"].'">';
+                                                echo '<dt>';
+                                                echo '<a href="./module_shop_singleshop.php?shop_name='.$item_search["shop_name"].'&shop_keeper='.$item_search["shop_keeper"].'">';
+                                                echo $item_search["shop_name"];
+                                                echo '</a>';
+                                                echo '</dt>';
+                                                echo '<dd>';
+                                                echo $item_search["shop_keeper"];
+                                                echo '</dd>';
+                                                echo '</dl>';
+                                                echo '<dl class="palette '.$item_search["shop_style_bottom"].'">';
+                                                echo '<dt>';
+                                                //echo '<a>';
+                                                //echo $item_search["shop_name"];
+                                                //echo '</a>';
+                                                echo '</dt>';
+                                                echo '<dd>';
+                                                echo $item_search["shop_keeper"];
+                                                echo '</dd>';
+                                                echo '</dl>';
+                                                echo '</div>';
+                                            }
+                                        }else{
+                                            echo '<div>';
+                                            echo "对不起，您所选择的商店种类暂时没有，请选择其他种类商店...";	
+                                            echo '</div>';
+                                        }
+                                        
+                                    }else{
+                                        echo '<div>';
+                                        echo "您未选择任何商店种类，请从左边列表选择...";
+                                        echo '</div>';
+                                    }
+                                ?>
+                            </div> <!-- /swatches items -->
+                        
+                    </div>
                 </div> <!-- /.col-xs-9 -->
+          
           		<div class="col-xs-1">
                     
                 </div><!-- /.col-xs-1-->
