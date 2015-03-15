6b7c22eb862137edbae151c16c466de9b47693d0
xuyongsky123 <xuyongsky123@163.com>
2015-03-15 09:55:17 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- module_shop_shop.php
+++ module_shop_shop.php
@@ -140,38 +140,6 @@
       </div>
       
       <?php include("./module_shop_templetfooter.php"); ?>
-      <footer class="footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-7">
-            <h3 class="footer-title">Free Style</h3>
-            <p>Do you know this site about Free Style? Want to get more info or give more suggestion like this?<br/>
-              Bring into action!!!<br/>
-              Go to: <a href="http://xuyongsky.sinaapp.com" target="_blank">Free Style</a>
-            </p>
-            <p class="pvl">
-                <h5>Open Online Store:</h5>
-              	<h6>We want you JOIN US:</h6>
-                <a target="blank" href="http://wpa.qq.com/msgrd?v=3&uin=1845082157&site=qq&menu=yes"><img border="0" src=http://wpa.qq.com/pa?p=1:1845082157:51 alt="点击这里给我发消息"></a>
-			  	<a target="blank" href="http://wpa.qq.com/msgrd?v=3&uin=821594487&site=qq&menu=yes"><img border="0" src=http://wpa.qq.com/pa?p=1:821594487:51 alt="点击这里给我发消息"></a>
-            </p>
-          </div> <!-- /col-xs-7 -->
-
-          <div class="col-xs-5">
-            <div class="footer-banner">
-              <h3 class="footer-title">College Mall</h3>
-              <ul>
-                <li>Item of Free Style</li>
-                <li>A Lot of Modules from Free Style</li>
-                <li>More Sections</li>
-                <li>Pro Module Swatches</li>
-                <li>Free Style Based Your JOIN!</li>
-              </ul>
-              Go to: <a href="http://xuyongsky.sinaapp.com" target="_blank">Free Style</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+      
   </body>
 </html>
