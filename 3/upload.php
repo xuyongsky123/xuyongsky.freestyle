0c0b50d291de30f948224beb2ddc8ac249d82dde
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-15 14:58:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/upload.php

--- 3/upload.php
+++ 3/upload.php
@@ -6,7 +6,7 @@
 			$article = addslashes(fread(fopen($data, "r"), filesize($data))); 
 			require("./database/connDB.inc.php");
 			
-            $sql="insert into shop_single_detail(shop_name,shop_keeper,goods_name,goods_price,goods_detail,goods_image,goods_style_top,goods_style_bottom) values('品牌手机小店','闭上眼 会有好梦吗','iPad','待商定价','iPad',$article,'palette-sun-flower','palette-orange')";
+            $sql="insert into shop_single_detail(shop_name,shop_keeper,goods_name,goods_price,goods_detail,goods_image,goods_style_top,goods_style_bottom) values('品牌手机小店','闭上眼 会有好梦吗','iPad','待商定价','iPad','$article','palette-sun-flower','palette-orange')";
             //$sql="insert into public_article(public_filename,public_article,public_author) values('$blog_title','$data','$user')";
 			$result=$operatedb->Execsql($sql,$conn);
 			
