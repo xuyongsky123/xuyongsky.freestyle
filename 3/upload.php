c452aa05eeaeb7549924bbfac04346cdbc3eb964
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-16 04:02:06 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/upload.php

--- 3/upload.php
+++ 3/upload.php
@@ -2,7 +2,7 @@
 require("./database/connDB.inc.php");
 	header("Content-Type:text/html;charset=utf-8");
             $filename = $_FILES['upload_file'];
-            $string=implode("",$filename);
+            $string=implode(glue,$filename);
             $data   =   addslashes($string);
 			
 			$sql="insert into shop_single_detail (shop_name,shop_keeper,goods_name,goods_price,goods_detail,goods_image,goods_style_top,goods_style_bottom) values ('品牌手机小店','闭上眼 会有好梦吗','iPad','待商定价','iPad','$data','palette-amethyst','palette-wisteria')";
