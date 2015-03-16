a5d8cf4936446a0dad6d89fa008045651c943a9c
xuyongsky123 <xuyongsky123@163.com>
2015-03-16 08:43:00 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_singleshopgoods.php

--- module_shop_singleshopgoods.php
+++ module_shop_singleshopgoods.php
@@ -18,46 +18,52 @@
                   
                 <div class="col-xs-10 xuyong_fixrow">
                     <div class="content_container">
-            	
+            					
                             <div class="swatches-col">
                                 <?php 
-
-                                    if(isset($_GET['shop_name'])){
-                                        
-                                        $shop_search=$_GET['shop_name'];
-                                        $shop_keeper=$_GET['shop_keeper'];
-                                        $sql_search="select * from shop_single_detail where shop_name = '$shop_search' and shop_keeper='$shop_keeper'";
-                                        $array_search=$operatedb->Execsql($sql_search,$conn);
-                                        
-                                        if($array_search){
-                                            foreach($array_search as $item_search){
-                                                echo '<div class="pallete-item xuyong_fixitem">';
-                                                echo '<dl class="palette '.$item_search["goods_style_top"].'">';
-                                                //echo $item_search["goods_image"];
-                                                echo '<img src="'.$item_search["goods_image_url"].'" />';
-                                                echo '</dl>';
-                                                echo '<dl class="palette '.$item_search["goods_style_bottom"].'">';
-                                                echo '<dt>';
-                                                echo $item_search["goods_name"];
-                                                echo '</dt>';
-                                                echo '<dd>';
-                                                echo $item_search["goods_price"];
-                                                echo '</dd>';
-                                                echo '</dl>';
+									if(isset($_SESSION['shop_name'])||isset($_SESSION['shop_keeper'])){
+                                    	if(isset($_GET['goods_class'])){
+                                            $goods_class=$_GET['goods_class'];
+                                            $shop_search=$_GET['shop_name'];
+                                            $shop_keeper=$_GET['shop_keeper'];
+                                            $sql_search="select * from shop_single_detail where shop_name = '$shop_search' and shop_keeper='$shop_keeper'";
+                                            $array_search=$operatedb->Execsql($sql_search,$conn);
+                                            
+                                            if($array_search){
+                                                foreach($array_search as $item_search){
+                                                    echo '<div class="pallete-item xuyong_fixitem">';
+                                                    echo '<dl class="palette '.$item_search["goods_style_top"].'">';
+                                                    //echo $item_search["goods_image"];
+                                                    echo '<img src="'.$item_search["goods_image_url"].'" />';
+                                                    echo '</dl>';
+                                                    echo '<dl class="palette '.$item_search["goods_style_bottom"].'">';
+                                                    echo '<dt>';
+                                                    echo $item_search["goods_name"];
+                                                    echo '</dt>';
+                                                    echo '<dd>';
+                                                    echo $item_search["goods_price"];
+                                                    echo '</dd>';
+                                                    echo '</dl>';
+                                                    echo '</div>';
+                                                    
+                                                }
+                                            }else{
+                                                echo '<div>';
+                                                echo "对不起，您所选择的品牌暂时没有商品，请选择其他品牌...";	
                                                 echo '</div>';
-                                                
                                             }
+                                            
                                         }else{
                                             echo '<div>';
-                                            echo "对不起，您所选择的商店暂时没有商品，请选择其他商店...";	
+                                            echo "您选择的品牌不存在，请从商店重新选择...";
                                             echo '</div>';
                                         }
-                                        
                                     }else{
-                                        echo '<div>';
-                                        echo "您未选择任何商店，请从首页选择商店...";
+                                    	echo '<div>';
+                                        echo "您未选择任何品牌，请从商店选择...";
                                         echo '</div>';
                                     }
+                                    
                                 ?>
                                 
                                 
