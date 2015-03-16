6fedce2715d78a3a505bc5b5f60179ee67089347
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-16 02:18:19 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/upload.php

--- 3/upload.php
+++ 3/upload.php
@@ -1,14 +1,14 @@
-
 <?php 
-	session_start();
+require("./database/connDB.inc.php");
 	header("Content-Type:text/html;charset=utf-8");
 			$file=$_FILES['upload_file'];
-			$article=addslashes($file);
+			$PSize = filesize($file); 
+			$article = addslashes(fread(fopen($file, "r"), $PSize)
 			
-			require("./database/connDB.inc.php");
 			
-                                  //$sql="insert into user_article(description,article,filename,filesize,filetype,username) values('test','$data','$blog_title','test','test','$user')";
-            $sql="insert into shop_single_detail(shop_name,shop_keeper,goods_name,goods_price,goods_detail,goods_image,goods_style_top,goods_style_bottom) values('品牌手机小店','闭上眼 会有好梦吗','iPad','待商定价','iPad','$article','palette-sun-flower','palette-orange')";
+			//$sql="insert into user_article(description,article,filename,filesize,filetype,username) values('test','$data','$blog_title','test','test','$user')";
+            $sql="insert into shop_single_detail(shop_name,shop_keeper,goods_name,goods_price,goods_detail,goods_image,goods_style_top,goods_style_bottom) 
+            							values('品牌手机小店','闭上眼 会有好梦吗','iPad','待商定价','iPad','$article','palette-sun-flower','palette-orange')";
             //$sql="insert into public_article(public_filename,public_article,public_author) values('$blog_title','$data','$user')";
 			$result=$operatedb->Execsql($sql,$conn);
 			
