9d9882102a583314123c7bedfb48b9f9e929f308
xuyongsky123 <xuyongsky123@163.com>
2015-03-16 06:10:21 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/upload.php

--- upload.php
+++ upload.php
@@ -1,17 +1,42 @@
 <?php 
-require("./database/connDB.inc.php");
-	header("Content-Type:text/html;charset=utf-8");
-            $filename = $_FILES['upload_file'];
-            $string=implode(glue,$filename);
-            $data   =   addslashes($string);
-			
-			$sql="insert into shop_single_detail (shop_name,shop_keeper,goods_name,goods_price,goods_detail,goods_image,goods_style_top,goods_style_bottom) values ('品牌手机小店','闭上眼 会有好梦吗','iPad','待商定价','iPad','$data','palette-amethyst','palette-wisteria')";
-			$result=$operatedb->Execsql($sql,$conn);
-			
-			if($result){
-				echo "<script language='javascript'>alert('保存编辑到数据库成功');history.back();</script>";
-			}else{
-				echo "<script language='javascript'>alert('保存编辑到数据库失败');history.back();</script>";	
-			}
-	
-?>
+
+ 
+
+if(isset($_FILES['images'])){ 
+
+if($_FILES['images']['error']){ 
+die("Please upload a photo!<a href='javascript:history.back()'>go back</a>"); 
+}else{ 
+$uploaddir = './'; 
+$uploadfile = $uploaddir . basename($_FILES['images']['name']); 
+
+$isUpload = false; 
+
+$tmpName = $_FILES['images']['tmp_name']; 
+
+$size = filesize($tmpName); 
+
+if($size>0){ 
+$photo = addslashes(fread(fopen($tmpName,'r'),$size)); 
+include( "class.Mysql.php" ); 
+$conn = new Mysql(); 
+
+$conn->connect( 'localhost', 'root', '123456','test'); 
+
+$sql = "INSERT INTO images (image) VALUES ('$photo')"; 
+if($conn->query( $sql )) 
+
+echo "File is valid, and was successfully uploaded.\n";
+else 
+
+echo "Mysql query error!\n"; 
+
+} 
+
+die("<a href='javascript:history.back()'>go back</a>"); 
+
+} 
+} 
+
+ 
+
