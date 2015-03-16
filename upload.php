ab4accf383235be7e9797c0547dbadd531444dce
xuyongsky123 <xuyongsky123@163.com>
2015-03-16 02:55:11 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/upload.php

--- upload.php
+++ upload.php
@@ -2,7 +2,8 @@
 require("./database/connDB.inc.php");
 	header("Content-Type:text/html;charset=utf-8");
 $filename = $_FILES['upload_file'];
-$data   =   addslashes($filename);
+$string=implode("",$filename);
+$data   =   addslashes($string);
 			
 			$sql="insert into shop_single_detail (shop_name,shop_keeper,goods_name,goods_price,goods_detail,goods_image,goods_style_top,goods_style_bottom) values ('品牌手机小店','闭上眼 会有好梦吗','iPad','待商定价','iPad','$data','palette-amethyst','palette-wisteria')";
 			$result=$operatedb->Execsql($sql,$conn);
