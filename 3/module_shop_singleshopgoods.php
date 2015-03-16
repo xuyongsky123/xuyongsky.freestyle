1af312a4042b226b5900a67755eb97e74fb09690
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-16 08:29:59 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_singleshopgoods.php

---
+++ 3/module_shop_singleshopgoods.php
@@ -0,0 +1,76 @@
+<?php
+  session_start();
+  require("./database/connDB.inc.php");
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head> 
+    <?php include('./frame_bootstrap_templethead.php'); ?>
+    <link href="./css/module_shop_css/module_shop_style.css" rel="stylesheet" />
+	<title><?php echo $_GET["shop_name"]; ?></title>
+  </head>
+  <body>
+      <?php include("./module_shop_templetheader.php"); ?>
+      
+      <div class="row demo-samples">
+                <div class="col-xs-1">
+                </div><!-- /.col-xs-1-->
+                  
+                <div class="col-xs-10 xuyong_fixrow">
+                    <div class="content_container">
+            	
+                            <div class="swatches-col">
+                                <?php 
+
+                                    if(isset($_GET['shop_name'])){
+                                        
+                                        $shop_search=$_GET['shop_name'];
+                                        $shop_keeper=$_GET['shop_keeper'];
+                                        $sql_search="select * from shop_single_detail where shop_name = '$shop_search' and shop_keeper='$shop_keeper'";
+                                        $array_search=$operatedb->Execsql($sql_search,$conn);
+                                        
+                                        if($array_search){
+                                            foreach($array_search as $item_search){
+                                                echo '<div class="pallete-item xuyong_fixitem">';
+                                                echo '<dl class="palette '.$item_search["goods_style_top"].'">';
+                                                //echo $item_search["goods_image"];
+                                                echo '<img src="'.$item_search["goods_image_url"].'" />';
+                                                echo '</dl>';
+                                                echo '<dl class="palette '.$item_search["goods_style_bottom"].'">';
+                                                echo '<dt>';
+                                                echo $item_search["goods_name"];
+                                                echo '</dt>';
+                                                echo '<dd>';
+                                                echo $item_search["goods_price"];
+                                                echo '</dd>';
+                                                echo '</dl>';
+                                                echo '</div>';
+                                                
+                                            }
+                                        }else{
+                                            echo '<div>';
+                                            echo "对不起，您所选择的商店暂时没有商品，请选择其他商店...";	
+                                            echo '</div>';
+                                        }
+                                        
+                                    }else{
+                                        echo '<div>';
+                                        echo "您未选择任何商店，请从首页选择商店...";
+                                        echo '</div>';
+                                    }
+                                ?>
+                                
+                                
+                            </div> <!-- /swatches items -->
+                        
+                    </div>
+                </div> <!-- /.col-xs-10 -->
+          		<div class="col-xs-1">
+                </div><!-- /.col-xs-1-->
+          
+      </div>
+      
+      <?php include("./module_shop_templetfooter.php"); ?>
+      
+  </body>
+</html>
