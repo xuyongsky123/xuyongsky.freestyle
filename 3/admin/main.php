ef26831ae383181976f918b86209e3820a0e73c2
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-16 11:00:14 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

---
+++ 3/admin/main.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html long="zh-CN">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        
+        <!--bootstrap_begin-->
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <!-- Bootstrap -->
+            <link href="../css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="../js/jquery-1.9.1.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="../js/bootstrap_js/bootstrap.min.js"></script>
+            
+            
+            <!-- Loading Flat UI -->
+            <link href="../css/bootstrap_css/flat_ui_css/flat-ui.css" rel="stylesheet" />
+            <link href="../css/bootstrap_css/flat_ui_css/demo.css" rel="stylesheet" />
+        <!--bootstrap_end-->
+        
+        <title>后台登陆界面</title>
+        <style type="text/css">
+        </style>	
+    </head>
+    <body>
+    	<div class="col-xs-9">
+          <div class="demo-browser">
+            <div class="demo-browser-side">
+              <div class="demo-browser-author"></div>
+              <div class="demo-browser-action">
+                <a class="btn btn-danger btn-lg btn-block" href="http://twitter.com/monstercritic" target="_blank">
+                  <span class="fui-plus"></span>Follow
+                </a>
+              </div>
+              <h5>@monstercritic</h5>
+              <h6>
+                Tourist. Designer. NYC
+                <a href="http://shmidt.in" target="_blank">shmidt.in</a>
+              </h6>
+            </div>
+            <div class="demo-browser-content">
+              <img src="docs/assets/img/demo/browser-pic-1.jpg" alt="" />
+              <img src="docs/assets/img/demo/browser-pic-2.jpg" alt="" />
+              <img src="docs/assets/img/demo/browser-pic-3.jpg" alt="" />
+              <img src="docs/assets/img/demo/browser-pic-4.jpg" alt="" />
+              <img src="docs/assets/img/demo/browser-pic-5.jpg" alt="" />
+              <img src="docs/assets/img/demo/browser-pic-6.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+    </body>
+</html>
