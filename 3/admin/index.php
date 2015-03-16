8ce8c395e9d79dba2f3240902a6994b35115dbb1
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-16 10:01:58 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/index.php

--- 3/admin/index.php
+++ 3/admin/index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html long="zh-CN">
     <head>
-        <?php require("../dynamic_effect_cloud.php") ?>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>后台登陆界面</title>
     <style type="text/css">
