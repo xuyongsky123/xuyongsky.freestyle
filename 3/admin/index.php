60a59ed247fa33851eea248eab6c729b96e8889f
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-16 10:16:57 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/index.php

--- 3/admin/index.php
+++ 3/admin/index.php
@@ -20,7 +20,7 @@
         #login_container{
             position:fixed;
             top:50%;
-            left:60%;
+            left:55%;
             height:200px;
             width:500px;
             background:#326696;
