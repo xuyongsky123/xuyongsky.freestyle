71f39dcc3716efd9198645bd7898152bed3d7022
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-16 07:21:49 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_singleshopshowlist.php

---
+++ 3/module_shop_singleshopshowlist.php
@@ -0,0 +1,24 @@
+<?php
+	require("./database/connDB.inc.php");
+		
+	$user=$_SESSION['username'];
+	$sql="select distinct goods_class from shop_single_detail";
+	$array=$operatedb->Execsql($sql,$conn);
+        
+    if($array){
+        foreach($array as $item){
+			echo '<li>';
+			echo '<div class="fix_style">';
+            echo '<h4 class="todo-name">';
+			echo '<a href="./module_shop_shop.php?shop_type='.$item["shop_type"].'">';
+			echo $item["shop_type"];
+			echo '</a>';
+			echo '</h4>';
+			echo '</div>';
+			echo '</li>';
+		}
+    }else{
+        echo "暂时还没有分类...";
+    }
+	
+?>
