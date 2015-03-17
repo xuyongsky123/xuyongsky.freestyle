03abadbe0868e460115a0f3f870126ab992c5c97
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-17 04:12:47 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- 3/admin/main.php
+++ 3/admin/main.php
@@ -53,10 +53,7 @@
                    <form class="navbar-form navbar-right" action="#" role="search">
                     <div class="form-group">
                       <div class="input-group">
-                        <input class="form-control" id="navbarInput-01" type="search" placeholder="Search">
-                        <span class="input-group-btn">
-                          <button type="submit" class="btn"><span class="fui-search"></span></button>
-                        </span>
+                          登录显示
                       </div>
                     </div>
                   </form>
