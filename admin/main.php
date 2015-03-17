0981af834e0828ee8fc837aca657a96f5c4c2a71
xuyongsky123 <xuyongsky123@163.com>
2015-03-17 03:49:24 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- admin/main.php
+++ admin/main.php
@@ -27,15 +27,51 @@
     </head>
     <body>
     	<div class="col-xs-12">
-              <div class="demo-browser">
-                    <div class="demo-browser-side">
-                      <div class="demo-browser-author"></div>
-                      <div class="demo-browser-action">
-                      </div>
+            <div class="col-xs-4">
+                  <div class="todo">
+                    <div class="todo-search">
+                      <input class="todo-search-field" type="search" value="" placeholder="Search" />
                     </div>
-                    <div class="demo-browser-content">
-                    </div>
-              </div>
+                    <ul>
+                      <li class="todo-done">
+                        <div class="todo-icon fui-user"></div>
+                        <div class="todo-content">
+                          <h4 class="todo-name">
+                            Meet <strong>Adrian</strong> at <strong>6pm</strong>
+                          </h4>
+                          Times Square
+                        </div>
+                      </li>
+                      <li>
+                        <div class="todo-icon fui-list"></div>
+                        <div class="todo-content">
+                          <h4 class="todo-name">
+                            Chat with <strong>V.Kudinov</strong>
+                          </h4>
+                          Skype conference an 9 am
+                        </div>
+                      </li>
+                      <li>
+                        <div class="todo-icon fui-eye"></div>
+                        <div class="todo-content">
+                          <h4 class="todo-name">
+                            Watch <strong>Iron Man</strong>
+                          </h4>
+                          1998 Broadway
+                        </div>
+                      </li>
+                      <li>
+                        <div class="todo-icon fui-time"></div>
+                        <div class="todo-content">
+                          <h4 class="todo-name">
+                            Fix bug on a <strong>Website</strong>
+                          </h4>
+                          As soon as possible
+                        </div>
+                      </li>
+                    </ul>
+                  </div><!-- /.todo -->
+              </div><!-- /.col-xs-4 -->
          </div>
     </body>
 </html>
