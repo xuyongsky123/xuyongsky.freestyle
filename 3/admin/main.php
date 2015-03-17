abda7d070b4c3b8815647b04c084fed03b909859
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-17 06:11:53 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- 3/admin/main.php
+++ 3/admin/main.php
@@ -9,7 +9,6 @@
             <!-- Bootstrap -->
             <link href="../css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
             <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-            <script src="../js/jquery-1.9.1.min.js"></script>
             <!-- Include all compiled plugins (below), or include individual files as needed -->
             <script src="../js/bootstrap_js/bootstrap.min.js"></script>
             
@@ -91,6 +90,7 @@
                             Fix bug on a <strong>Web2ite</strong>
                 
         <!--flat-ui's js begin-->
+            <script src="../js/jquery-1.9.1.min.js"></script>
         	<script src="../js/bootrap_js/flat_ui_js/video.js"></script>
         	<script src="../js/bootrap_js/flat_ui_js/flat-ui.min.js"></script>
             <script src="../js/bootrap_js/flat_ui_js/application.js"></script>
