6a3eaa81e7a905bda4763f9abf3f98faf572f738
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-17 03:39:32 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- 3/admin/main.php
+++ 3/admin/main.php
@@ -21,7 +21,7 @@
         
         	<link href="./css/main_style.css" rel="stylesheet" />
         
-        <title>后台界面</title>
+        <title>后台主界面</title>
         <style type="text/css">
         </style>	
     </head>
