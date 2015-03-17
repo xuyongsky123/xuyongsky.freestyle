3230598fed65d87f9ff72d9bb892d5e0a7e2f68c
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-17 08:20:05 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- 3/admin/main.php
+++ 3/admin/main.php
@@ -15,24 +15,21 @@
             <link href="../css/bootstrap_css/flat_ui_css/demo.css" rel="stylesheet" />
         <!--bootstrap_end-->
         
-        	<link href="./css/main_style.css" rel="stylesheet" />
-        
         <!-- Le styles -->
+        
             <link type="text/css" href="css/custom-theme/jquery-ui-1.10.0.custom.css" rel="stylesheet" />
-            <link type="text/css" href="assets/css/font-awesome.min.css" rel="stylesheet" />
             <!--[if IE 7]>
             <link rel="stylesheet" href="assets/css/font-awesome-ie7.min.css">
             <![endif]-->
             <!--[if lt IE 9]>
             <link rel="stylesheet" type="text/css" href="css/custom-theme/jquery.ui.1.10.0.ie.css"/>
             <![endif]-->
-            <link href="assets/css/docs.css" rel="stylesheet">
-            <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
 
             <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
             <!--[if lt IE 9]>
             <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
             <![endif]-->
+        	<link href="./css/main_style.css" rel="stylesheet" />
 
         <title>后台主界面</title>
         <style type="text/css">
