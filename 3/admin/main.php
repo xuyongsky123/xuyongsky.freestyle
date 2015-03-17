a54507aba46ca486a855d4c4c4f9524ba380bcf5
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-17 07:25:25 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- 3/admin/main.php
+++ 3/admin/main.php
@@ -134,7 +134,7 @@
 
 	<script src="media/js/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>      
 
-	<script src="media/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="../js/bootstrap_js/bootstrap.min.js"></script>
 
 	<!--[if lt IE 9]>
 
