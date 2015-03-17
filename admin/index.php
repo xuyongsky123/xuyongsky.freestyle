7ba55c41b1ef6802c45183d405df7d01a6d1ce76
xuyongsky123 <xuyongsky123@163.com>
2015-03-17 02:35:49 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -89,8 +89,8 @@
             clear:both;
         }
         input:-webkit-autofill,
-    textarea:-webkit-autofill,
-    select:-webkit-autofill{
+    	textarea:-webkit-autofill,
+    	select:-webkit-autofill{
             -webkit-box-shadow: 0 0 0 1000px transparent inset;
         }
 	</style>
