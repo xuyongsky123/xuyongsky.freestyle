60f078f577465e707d7122e0c7eea70806ac70c3
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-17 07:38:19 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- 3/admin/main.php
+++ 3/admin/main.php
@@ -19,6 +19,7 @@
             <link href="../css/bootstrap_css/flat_ui_css/demo.css" rel="stylesheet" />
         <!--bootstrap_end-->
         
+        	<link href="./css/main_style.css" rel="stylesheet" />
         
         <title>后台主界面</title>
         <style type="text/css">
@@ -49,6 +50,49 @@
                 </div><!-- /.navbar-collapse -->
             </nav><!-- /navbar -->
         </div>
+    	<div class="col-xs-12 col-xs-xuyong">
+            <div class="col-xs-2">
+                  <div class="todo">
+                    <div class="todo-search">
+                      <input class="todo-search-field" type="search" readonly="readonly" value="" placeholder="Search" />
+                    </div>
+                    <ul>
+                      <li class="todo-done">
+                        <div class="todo-content">
+                          <h4 class="todo-name">
+                            Meet <strong>Adrian</strong> at <strong>6pm</strong>
+                          </h4>
+                          Times Square
+                        </div>
+                      </li>
+                      <li>
+                        <div class="todo-content">
+                          <h4 class="todo-name">
+                            Chat with <strong>V.Kudinov</strong>
+                          </h4>
+                          Skype conference an 9 am
+                        </div>
+                      </li>
+                      <li>
+                        <div class="todo-content">
+                          <h4 class="todo-name">
+                            Watch <strong>Iron Man</strong>
+                          </h4>
+                          1998 Broadway
+                        </div>
+                      </li>
+                      <li>
+                        <div class="todo-content">
+                          <h4 class="todo-name">
+                            Fix bug on a <strong>Website</strong>
+                          </h4>
+                          As soon as possible
+                        </div>
+                      </li>
+                    </ul>
+                  </div><!-- /.todo -->
+              </div><!-- /.col-xs-2 -->
+         </div>
         
         <!--flat-ui's js begin-->
         	<script src="../js/bootstrap_js/flat_ui_js/flat-ui.min.js"></script>
