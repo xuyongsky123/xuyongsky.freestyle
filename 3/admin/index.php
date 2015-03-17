0d4d9c59090a6ada74820df56f87c6f842babdec
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-17 03:10:00 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/index.php

--- 3/admin/index.php
+++ 3/admin/index.php
@@ -104,6 +104,25 @@
             font-weight:bold;
         }
 	</style>
+    <script>
+            function checkUser(){
+                var Username=document.getElementById('login_username').value;
+                var Password=document.getElementById('login_password').value;
+                
+                if(Username==""||Username==null){
+                    alert("The Username can't be null");
+                    return false;
+                }
+                if(Password==""||Password==null){
+                    alert("The Password can't be null");
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+		</script>
     </head>
     <body>
         <div id="login_container">
