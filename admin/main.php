ff86fcf86cf9fb021e4eca3bdbcc0b67c067f9fe
xuyongsky123 <xuyongsky123@163.com>
2015-03-18 07:02:00 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- admin/main.php
+++ admin/main.php
@@ -53,7 +53,7 @@
                 <div class="collapse navbar-collapse nav_xuyong" id="navbar-collapse-01">
                   <ul class="nav navbar-nav navbar-left">
                     <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">欢迎您，<?php echo $_SESSION['username']; ?><b class="caret"></b></a>
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">欢迎您，<?php echo $_SESSION['username_adminuserfixpassword__']; ?><b class="caret"></b></a>
                       <span class="dropdown-arrow"></span>
                       <ul class="dropdown-menu">
                         <li><a href="#">Action</a></li>
