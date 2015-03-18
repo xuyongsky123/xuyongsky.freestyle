e59011a6b1a76bc6bd20b5217d2dd5ca3a546768
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-18 10:28:20 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- 3/admin/info_shop.php
+++ 3/admin/info_shop.php
@@ -35,7 +35,7 @@
                 <li><a href="#tabs-d">删除商店信息</a></li>
             </ul>
             <div id="tabs-a">
-                <a href="#" id="select_shop">按钮<?php echo $_SESSION['username_adminuserfixpassword__']; ?></a>
+                <a href="http://www.baidu.com" id="select_shop">按钮<?php echo $_SESSION['username_adminuserfixpassword__']; ?></a>
                 <div id="select_content"></div>
             </div>
             <div id="tabs-b">Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.....</div>
