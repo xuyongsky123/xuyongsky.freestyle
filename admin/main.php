15ad24633755145cf8963d8f1d051eb87243cfa4
xuyongsky123 <xuyongsky123@163.com>
2015-03-18 06:22:10 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- admin/main.php
+++ admin/main.php
@@ -1,6 +1,6 @@
 <?php  
 	session_start();
-    if(!isset($_SESSION['username'])){
+    if(isset($_SESSION['username'])){
         echo "<script language='javascript'>alert('请先登录...');window.location.href='./index.php';</script>";
     }
 ?>
