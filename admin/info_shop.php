9ef3d564382cf22426c482840373bc9cae7aa6ad
xuyongsky123 <xuyongsky123@163.com>
2015-03-19 06:11:06 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- admin/info_shop.php
+++ admin/info_shop.php
@@ -2,23 +2,6 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
     <head>
-        <title>商店信息管理</title>
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-            <link href="../css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
-            
-            
-            <!-- Loading Flat UI -->
-            <link href="../css/bootstrap_css/flat_ui_css/flat-ui.css" rel="stylesheet" />
-            <link href="../css/bootstrap_css/flat_ui_css/demo.css" rel="stylesheet" />
-        <!--bootstrap_end-->
-        
-        <!-- Le styles -->
-        
-        	<link type="text/css" href="./css/jquery-ui-1.10.0.custom.css" rel="stylesheet" />
-        
-        	
-        	<link href="./css/main_style.css" rel="stylesheet" />
-        	
     </head>
     <body>
         <div id="tabs">
