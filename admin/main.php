a4103fbf815ae0126dc69efa99cba5ed20920434
xuyongsky123 <xuyongsky123@163.com>
2015-03-19 03:33:59 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- admin/main.php
+++ admin/main.php
@@ -127,7 +127,7 @@
             <script src="./assets/js/docs.js" type="text/javascript"></script>
             <script src="./assets/js/demo.js" type="text/javascript"></script>
         
-        <script src=".。/js/bootstrap_js/flat_ui_js/flat-ui.min.js"></script>
+        <script src="../js/bootstrap_js/flat_ui_js/flat-ui.min.js"></script>
             <script src="docs/assets/js/application.js"></script>
     </body>
 </html>
