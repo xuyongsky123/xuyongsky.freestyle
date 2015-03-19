4021621159507f5d4d34610b000272757c2cf1af
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-19 05:53:10 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- 3/admin/main.php
+++ 3/admin/main.php
@@ -41,22 +41,6 @@
         	<link href="./css/main_style.css" rel="stylesheet" />
 
         <title>后台主界面</title>
-        
-        <!-- Placed at the end of the document so the pages load faster -->
-        
-            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-            <script src="../js/jquery-1.9.1.min.js"></script>
-            <!-- Include all compiled plugins (below), or include individual files as needed -->
-            <script src="../js/bootstrap_js/bootstrap.min.js"></script>
-            <script src="./assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
-            <script src="./assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
-            <script src="./assets/js/docs.js" type="text/javascript"></script>
-            <script src="./assets/js/demo.js" type="text/javascript"></script>
-        
-        	<script src="../js/bootstrap_js/flat_ui_js/flat-ui.min.js"></script>
-        
-        
-        
         <style type="text/css">
         </style>	
     </head>
@@ -131,7 +115,18 @@
         </div>
         
         
+        <!-- Placed at the end of the document so the pages load faster -->
         
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="../js/jquery-1.9.1.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="../js/bootstrap_js/bootstrap.min.js"></script>
+            <script src="./assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
+            <script src="./assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
+            <script src="./assets/js/docs.js" type="text/javascript"></script>
+            <script src="./assets/js/demo.js" type="text/javascript"></script>
+        
+        	<script src="../js/bootstrap_js/flat_ui_js/flat-ui.min.js"></script>
     </body>
 </html>
 <?php
