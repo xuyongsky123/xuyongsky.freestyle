b32273905669939fe6282a9d0a7a05dd93ab98cf
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-19 06:22:16 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/js_file.php

--- 3/admin/js_file.php
+++ 3/admin/js_file.php
@@ -1,6 +1,10 @@
 <?php session_start(); ?>
-
-<link href="../css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
+<!DOCTYPE html>
+<html lang="zh-CN">
+    <head>
+        <title>商店信息管理</title>
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+            <link href="../css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
             
             
             <!-- Loading Flat UI -->
@@ -11,32 +15,18 @@
         <!-- Le styles -->
         
         	<link type="text/css" href="./css/jquery-ui-1.10.0.custom.css" rel="stylesheet" />
-            <!--[if IE 7]>
-            <link rel="stylesheet" href="assets/css/font-awesome-ie7.min.css">
-            <![endif]-->
-            <!--[if lt IE 9]>
-            <link rel="stylesheet" type="text/css" href="css/custom-theme/jquery.ui.1.10.0.ie.css"/>
-            <![endif]-->
-
-            <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-            <!--[if lt IE 9]>
-            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-            <![endif]-->
         
-        
+        	
         	<link href="./css/main_style.css" rel="stylesheet" />
-
-
-<!-- Placed at the end of the document so the pages load faster -->
-        
-            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-            <script src="../js/jquery-1.9.1.min.js"></script>
-            <!-- Include all compiled plugins (below), or include individual files as needed -->
-            <script src="../js/bootstrap_js/bootstrap.min.js"></script>
-            <script src="./assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
-            <script src="./assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
-            <script src="./assets/js/docs.js" type="text/javascript"></script>
-            <script src="./assets/js/demo.js" type="text/javascript"></script>
-        
-        	<script src="../js/bootstrap_js/flat_ui_js/flat-ui.min.js"></script>
-        	<script src="../js/bootstrap_js/flat_ui_js/application.js"></script>
+        	
+    </head>
+    <body>
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="../js/jquery-1.9.1.min.js"></script>
+        <script src="../js/bootstrap_js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
+        <script src="assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
+        <script src="assets/js/docs.js" type="text/javascript"></script>
+        <script src="assets/js/demo.js" type="text/javascript"></script>
+    </body>    
+<html>
