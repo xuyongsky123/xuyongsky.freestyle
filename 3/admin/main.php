fdeddc58b2646716c41effa8b4966019a14aaa8f
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-19 06:42:25 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- 3/admin/main.php
+++ 3/admin/main.php
@@ -122,7 +122,6 @@
             <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
             <script src="../js/jquery-1.9.1.min.js"></script>
             <!-- Include all compiled plugins (below), or include individual files as needed -->
-            <script src="../js/bootstrap_js/bootstrap.min.js"></script>
         
         	<script src="../js/bootstrap_js/flat_ui_js/flat-ui.min.js"></script>
         	<script src="../js/bootstrap_js/flat_ui_js/application.js"></script>
