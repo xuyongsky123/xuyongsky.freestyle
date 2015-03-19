a8da2500737527ff1a832b629eb6e41f87c01bb3
xuyongsky123 <xuyongsky123@163.com>
2015-03-19 01:31:59 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- admin/main.php
+++ admin/main.php
@@ -90,12 +90,20 @@
                             </div>
                             <div class="title_style">
                                 <h3><a href="#">大学电商专栏</a></h3>
-                                <div class="fix_style">
-                                    <div class="college_mall_item"><a href="./info_shop.php" id="info_shop" name="info_shop.php" >商店信息管理</a></div>
+                                <!--<div class="fix_style">
+                                    <div class="college_mall_item"><a href="./info_shop.php" id="info_shop" name="info_shop.php" target="college_mall">商店信息管理</a></div>
                                     <div class="college_mall_item"><a href="./info_goods.php" id="info_goods" name="info_goods.php" target="college_mall">商品信息管理</a></div>
                                     <div class="college_mall_item"><a href="./info_user.php" id="info_user" name="info_user.php" target="college_mall">用户信息管理</a></div>
                                     <div class="college_mall_item"><a href="./info_member.php" id="info_member" name="info_member.php" target="college_mall">会员信息管理</a></div>
                                     <div class="college_mall_item"><a href="./info_order.php" id="info_order" name="info_order.php" target="college_mall">订单信息管理</a></div>
+                                    <div class="college_mall_item"><a href="#" id="" name="" target="college_mall">...</a></div>
+                                </div>-->
+                                <div class="fix_style">
+                                    <div class="college_mall_item"><a href="./info_shop.php" id="info_shop" name="info_shop.php">商店信息管理</a></div>
+                                    <div class="college_mall_item"><a href="./info_goods.php" id="info_goods" name="info_goods.php">商品信息管理</a></div>
+                                    <div class="college_mall_item"><a href="./info_user.php" id="info_user" name="info_user.php">用户信息管理</a></div>
+                                    <div class="college_mall_item"><a href="./info_member.php" id="info_member" name="info_member.php">会员信息管理</a></div>
+                                    <div class="college_mall_item"><a href="./info_order.php" id="info_order" name="info_order.php">订单信息管理</a></div>
                                     <div class="college_mall_item"><a href="#" id="" name="" target="college_mall">...</a></div>
                                 </div>
                             </div>
