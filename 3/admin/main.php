d18f20c1923a5f77a3e71657296d387990e1146e
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-19 06:13:28 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- 3/admin/main.php
+++ 3/admin/main.php
@@ -111,24 +111,11 @@
                 </div>
                 <div id="action_container" class="col-xs-10 content_container">
                     <!--<iframe src="" id="iframe" name="college_mall" scrolling="no">您的浏览器尚不支持iframe标签，请使用其他类型浏览器来浏览本页面...</iframe>-->
-                    <?php include('./info_shop.php'); ?>
         		</div>
         </div>
         
         
-        <!-- Placed at the end of the document so the pages load faster -->
-        
-            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-            <script src="../js/jquery-1.9.1.min.js"></script>
-            <!-- Include all compiled plugins (below), or include individual files as needed -->
-            <script src="../js/bootstrap_js/bootstrap.min.js"></script>
-            <script src="./assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
-            <script src="./assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
-            <script src="./assets/js/docs.js" type="text/javascript"></script>
-            <script src="./assets/js/demo.js" type="text/javascript"></script>
-        
-        	<script src="../js/bootstrap_js/flat_ui_js/flat-ui.min.js"></script>
-        	<script src="../js/bootstrap_js/flat_ui_js/application.js"></script>
+        <?php include("./js_file.php"); ?>
     </body>
 </html>
 <?php
