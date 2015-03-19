c9174217dd8ac44414fd159f1fb112dc83512484
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-19 06:50:02 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/js_file.php

--- 3/admin/js_file.php
+++ 3/admin/js_file.php
@@ -1,2 +1,27 @@
-
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+    <head>
+        <title>商店信息管理</title>
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+            <link href="../css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
+            
+            
+            <!-- Loading Flat UI -->
+            <link href="../css/bootstrap_css/flat_ui_css/flat-ui.css" rel="stylesheet" />
+            <link href="../css/bootstrap_css/flat_ui_css/demo.css" rel="stylesheet" />
+        <!--bootstrap_end-->
+        
+        <!-- Le styles -->
+        
+        	<link type="text/css" href="./css/jquery-ui-1.10.0.custom.css" rel="stylesheet" />
+        
+        	
+        	<link href="./css/main_style.css" rel="stylesheet" />
+        	
+    </head>
+    <body>
+        <!-- Placed at the end of the document so the pages load faster -->
         	<script src="../js/bootstrap_js/flat_ui_js/flat-ui.min.js"></script>
+    </body>    
+<html>
