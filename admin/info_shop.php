1754490920f4b07a6688a793e905f0c50f8a4cda
xuyongsky123 <xuyongsky123@163.com>
2015-03-19 10:05:21 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- admin/info_shop.php
+++ admin/info_shop.php
@@ -6,10 +6,10 @@
     <body>
     	<div id="group_item">
             <ul>
-                <li><a href="#tabs-a" rel="select_item">查询商店信息</a></li>
-                <li><a href="#tabs-b" rel="modify_item">修改商店信息</a></li>
-                <li><a href="#tabs-c" rel="add_item">增加商店信息</a></li>
-                <li><a href="#tabs-d" rel="delete_item">删除商店信息</a></li>
+                <li><a href="#tabs-a" class="item_click" rel="select_item">查询商店信息</a></li>
+                <li><a href="#tabs-b" class="item_click" rel="modify_item">修改商店信息</a></li>
+                <li><a href="#tabs-c" class="item_click" rel="add_item">增加商店信息</a></li>
+                <li><a href="#tabs-d" class="item_click" rel="delete_item">删除商店信息</a></li>
             </ul>
             <div id="select_item" class="group_content">
                 <a href="http://www.baidu.com" id="select_shop">按钮<?php echo $_SESSION['username_adminuserfixpassword__']; ?></a>
@@ -19,7 +19,6 @@
             <div id="add_item" class="group_content">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
             <div id="delete_item" class="group_content">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
         </div> 
-        <div id="test" style="background:red;height:100px;width:100px;"></div>
         
         <script src="../js/jquery-1.9.1.min.js"></script>
         <script src="./js/action.js"></script>
