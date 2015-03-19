9e4081a1f7293cad5ab2c00b44c74947d4b61e3d
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-19 10:29:57 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/js/action.js

--- 3/admin/js/action.js
+++ 3/admin/js/action.js
@@ -1,6 +1,10 @@
 $(function(){
 	$(".item_click").bind("click",function(){
     	var action=$(this).attr("rel");
+        alert(action);
+        alert("#"+action);
         $("#"+action).addClass("show");
+        var a=$("#"+action).attr("class");
+        alert(a);
     });
 });
