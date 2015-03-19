80ba52ef2a900a11531897b9dd4d8b6c81a1fa72
xuyongsky123 <xuyongsky123@163.com>
2015-03-19 04:04:45 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- admin/info_shop.php
+++ admin/info_shop.php
@@ -3,6 +3,21 @@
 <html lang="zh-CN">
     <head>
         <title>商店信息管理</title>
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+            <link href="../css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
+            
+            
+            <!-- Loading Flat UI -->
+            <link href="../css/bootstrap_css/flat_ui_css/flat-ui.css" rel="stylesheet" />
+            <link href="../css/bootstrap_css/flat_ui_css/demo.css" rel="stylesheet" />
+        <!--bootstrap_end-->
+        
+        <!-- Le styles -->
+        
+        	<link type="text/css" href="./css/jquery-ui-1.10.0.custom.css" rel="stylesheet" />
+        
+        	
+        	<link href="./css/main_style.css" rel="stylesheet" />
         	
     </head>
     <body>
@@ -23,6 +38,12 @@
         </div> 
         
         <!-- Placed at the end of the document so the pages load faster -->
+        <script src="../js/jquery-1.9.1.min.js"></script>
+        <script src="../js/bootstrap_js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
+        <script src="assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
+        <script src="assets/js/docs.js" type="text/javascript"></script>
+        <script src="assets/js/demo.js" type="text/javascript"></script>
     </body>    
 <html>
     
