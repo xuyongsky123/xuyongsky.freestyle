a79bfdabe7ac4087766779c58137be8c40427f6c
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-19 04:25:17 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- 3/admin/info_shop.php
+++ 3/admin/info_shop.php
@@ -1,26 +1,4 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="zh-CN">
-    <head>
-        <title>商店信息管理</title>
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-            <link href="../css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
-            
-            
-            <!-- Loading Flat UI -->
-            <link href="../css/bootstrap_css/flat_ui_css/flat-ui.css" rel="stylesheet" />
-            <link href="../css/bootstrap_css/flat_ui_css/demo.css" rel="stylesheet" />
-        <!--bootstrap_end-->
-        
-        <!-- Le styles -->
-        
-        	<link type="text/css" href="./css/jquery-ui-1.10.0.custom.css" rel="stylesheet" />
-        
-        	
-        	<link href="./css/main_style.css" rel="stylesheet" />
-        	
-    </head>
-    <body>
+
         <div id="tabs">
             <ul>
                 <li><a href="#tabs-a">查询商店信息</a></li>
@@ -36,15 +14,5 @@
             <div id="tabs-c">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
             <div id="tabs-d">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
         </div> 
-        
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="../js/jquery-1.9.1.min.js"></script>
-        <script src="../js/bootstrap_js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
-        <script src="assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
-        <script src="assets/js/docs.js" type="text/javascript"></script>
-        <script src="assets/js/demo.js" type="text/javascript"></script>
-    </body>    
-<html>
     
     
