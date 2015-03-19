99da50f51c0dc25dbb9d4e8f1a23a89d2285355a
xuyongsky123 <xuyongsky123@163.com>
2015-03-19 03:46:38 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/js/main_js.js

--- admin/js/main_js.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    
-    //edit by xuyong -begin
-    $(".college_mall_item a").bind("click",function(){
-        var item=$(this).attr("name");
-        alert(item);
-    });
-    //edit by xuyong -end
-});
