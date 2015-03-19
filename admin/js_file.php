119b016f478dae771eaa3d7d1f61644e1202828a
xuyongsky123 <xuyongsky123@163.com>
2015-03-19 06:48:19 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/js_file.php

--- admin/js_file.php
+++ admin/js_file.php
@@ -22,11 +22,6 @@
     </head>
     <body>
         <!-- Placed at the end of the document so the pages load faster -->
-        <script src="../js/jquery-1.9.1.min.js"></script>
-        <script src="../js/bootstrap_js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
-        <script src="assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
-        <script src="assets/js/docs.js" type="text/javascript"></script>
-        <script src="assets/js/demo.js" type="text/javascript"></script>
+        	<script src="../js/bootstrap_js/flat_ui_js/flat-ui.min.js"></script>
     </body>    
 <html>
