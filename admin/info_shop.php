a6f767f8f3f283668c9a40efc92364be0c005f80
xuyongsky123 <xuyongsky123@163.com>
2015-03-19 06:59:02 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- admin/info_shop.php
+++ admin/info_shop.php
@@ -1,10 +1,4 @@
- <!-- Placed at the end of the document so the pages load faster -->
-        <script src="../js/jquery-1.9.1.min.js"></script>
-        <script src="../js/bootstrap_js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
-        <script src="assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
-        <script src="assets/js/docs.js" type="text/javascript"></script>
-        <script src="assets/js/demo.js" type="text/javascript"></script>
+
         <div id="tabs">
             <ul>
                 <li><a href="#tabs-a">查询商店信息</a></li>
