37914d74702d01a2dfa1a28637e771ce458ac81d
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-19 10:38:49 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- 3/admin/info_shop.php
+++ 3/admin/info_shop.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
     <head>
-    	
+        <style>
+            .group_content{
+                display:none;
+            }
+            .show{
+                display:block;
+            }
+        </style>
     </head>
     <body>
     	<div id="group_item">
