f5cc6cb6307b95effb2fc1e5474f0d8c2496e4a7
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-19 10:01:58 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- 3/admin/info_shop.php
+++ 3/admin/info_shop.php
@@ -11,13 +11,13 @@
                 <li><a href="#tabs-c" rel="add_item">增加商店信息</a></li>
                 <li><a href="#tabs-d" rel="delete_item">删除商店信息</a></li>
             </ul>
-            <div id="select_item">
+            <div id="select_item" class="group_content">
                 <a href="http://www.baidu.com" id="select_shop">按钮<?php echo $_SESSION['username_adminuserfixpassword__']; ?></a>
                 <div id="select_content"></div>
             </div>
-            <div id="modify_item">Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.....</div>
-            <div id="add_item">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
-            <div id="delete_item">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
+            <div id="modify_item" class="group_content">Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.....</div>
+            <div id="add_item" class="group_content">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
+            <div id="delete_item" class="group_content">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
         </div> 
         <div id="test" style="background:red;height:100px;width:100px;"></div>
         
