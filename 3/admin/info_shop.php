988f9477a02ab275abcc801eef242fcbfae4a016
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-19 04:05:42 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- 3/admin/info_shop.php
+++ 3/admin/info_shop.php
@@ -18,6 +18,14 @@
         
         	
         	<link href="./css/main_style.css" rel="stylesheet" />
+        
+        	<!-- Placed at the end of the document so the pages load faster -->
+        <script src="../js/jquery-1.9.1.min.js"></script>
+        <script src="../js/bootstrap_js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
+        <script src="assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
+        <script src="assets/js/docs.js" type="text/javascript"></script>
+        <script src="assets/js/demo.js" type="text/javascript"></script>
         	
     </head>
     <body>
@@ -37,7 +45,7 @@
             <div id="tabs-d">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
         </div> 
         
-        <!-- Placed at the end of the document so the pages load faster -->
+        
     </body>    
 <html>
     
