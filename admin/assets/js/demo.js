4f148c590487718930071650928867c74bfb59d6
xuyongsky123 <xuyongsky123@163.com>
2015-03-19 03:06:47 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/assets/js/demo.js

--- admin/assets/js/demo.js
+++ admin/assets/js/demo.js
@@ -15,15 +15,7 @@ $(function () {
         header: "h3"
     });
     
-    
-    
-    //edit by xuyong -begin
-    $(".college_mall_item a").bind("click",function(){
-        var item=$(this).attr("name");
-        alert(item);
-    });
-    //edit by xuyong -end
-
+  
     
     
     // Dialog Link
