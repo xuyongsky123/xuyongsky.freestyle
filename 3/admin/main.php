29c05f25ea799e873aaaa3762f97496f26d80693
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-19 03:07:18 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/main.php

--- 3/admin/main.php
+++ 3/admin/main.php
@@ -126,6 +126,7 @@
             <script src="./assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
             <script src="./assets/js/docs.js" type="text/javascript"></script>
             <script src="./assets/js/demo.js" type="text/javascript"></script>
+            <script src="./js/main_js.js" type="text/javascript"></script>
     </body>
 </html>
 <?php
