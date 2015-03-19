975bace311b8c87f66af58157b6b6ad572506406
xuyongsky123 <xuyongsky123@163.com>
2015-03-19 10:10:09 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/js/action.js

--- admin/js/action.js
+++ admin/js/action.js
@@ -1,5 +1,9 @@
 $(function(){
 	$(".item_click").bind("click",function(){
-    	
+    	var action=$(this).attr("rel");
+        var actionto=$(".group_content").attr("id");
+        if(action==actionto){
+        	actionto.addClass("show");
+        }
     });
 });
