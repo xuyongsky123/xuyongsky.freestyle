329b1d35567911378be827a3319a2e3558338b17
xuyongsky123 <xuyongsky123@163.com>
2015-03-20 07:07:59 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop_show.php

--- admin/info_shop_show.php
+++ admin/info_shop_show.php
@@ -1,6 +1,6 @@
 <?php   
 header("Content-Type:text/html;charset=utf-8");
-if(!isset($_GET["shop_naem"]))
+if(!isset($_POST["shop_naem"]))
     echo "没有数据";
-    echo $_GET["shop_name"];
+    echo $_POST["shop_name"];
 ?>  
