e210d21285c0521d204c25b18e929834b65755d4
xuyongsky123 <xuyongsky123@163.com>
2015-03-20 07:59:20 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop_show.php

--- admin/info_shop_show.php
+++ admin/info_shop_show.php
@@ -21,20 +21,6 @@
             <link href="../css/bootstrap_css/flat_ui_css/flat-ui.css" rel="stylesheet" />
         <!--bootstrap_end-->
         
-        <!-- Le styles -->
-            <!--[if IE 7]>
-            <link rel="stylesheet" href="assets/css/font-awesome-ie7.min.css">
-            <![endif]-->
-            <!--[if lt IE 9]>
-            <link rel="stylesheet" type="text/css" href="css/custom-theme/jquery.ui.1.10.0.ie.css"/>
-            <![endif]-->
-
-            <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-            <!--[if lt IE 9]>
-            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-            <![endif]-->
-        
-        
         	<link href="./css/main_style.css" rel="stylesheet" />
         
         
