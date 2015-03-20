6bb1653c86aff687d50c6b6d0ee0011550d9c0fc
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-20 09:03:44 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop_show.php

--- 3/admin/info_shop_show.php
+++ 3/admin/info_shop_show.php
@@ -46,10 +46,11 @@
         <div id="content_show" style="width:100%;height:450px;background;">
         	<?php  
         		require("./database/connDB.inc.php");
-        		if(!isset($_POST['shop_identify']))
+        		$identify=$_POST['shop_identify'];
+        		if(!isset($identify))
                 {
                     echo "<script type='text/javascript'>alert('数据操作失败，请重新操作...');window.location.href='./main.php';</script>";
-                }else{
+                }else if($_POST){
                 	
                 }
             ?>
