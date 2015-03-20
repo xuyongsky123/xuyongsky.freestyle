01265006ca1ba4fa446f8cf7db7c4d80f3897506
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-20 07:49:54 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop_show.php

--- 3/admin/info_shop_show.php
+++ 3/admin/info_shop_show.php
@@ -53,14 +53,8 @@
                 </div>
                 <div class="collapse navbar-collapse nav_xuyong" id="navbar-collapse-01">
                       <button id="menu-button" type="button" class="btn btn-default dropdown-toggle menu-button" data-toggle="dropdown" aria-expanded="false">
-                        Action <span class="caret"></span>
+                          <a href="./main.php">返回</a>
                       </button>
-                      <ul class="dropdown-menu xuyong-dropdown-menu" role="menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Somethingfdsssssssssssssssssssssssssss</a></li>
-                        <li><a href="./check_admin_login.php?action=logout_admin">注销</a></li>
-                      </ul>
                 </div><!-- /.navbar-collapse -->
             </nav><!-- /navbar -->
         </div>
