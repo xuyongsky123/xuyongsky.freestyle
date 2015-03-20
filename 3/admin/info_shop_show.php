4076dc4e6d50d8c0c29214e8da4a5c12d3a9b81e
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-20 07:56:04 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop_show.php

--- 3/admin/info_shop_show.php
+++ 3/admin/info_shop_show.php
@@ -62,6 +62,12 @@
         </div>
       
         
+        <!-- Placed at the end of the document so the pages load faster -->
+        
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="../js/jquery-1.9.1.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="../js/bootstrap_js/bootstrap.min.js"></script>
         
     </body>
 </html>
