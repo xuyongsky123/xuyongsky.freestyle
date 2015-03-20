41d0c93c098a0abe94c044e4220d2c82f60d9978
xuyongsky123 <xuyongsky123@163.com>
2015-03-20 02:48:37 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_member.php

--- admin/info_member.php
+++ admin/info_member.php
@@ -2,14 +2,6 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
     <head>
-        <style>
-            .group_content{
-                display:none;
-            }
-            .show{
-                display:block;
-            }
-        </style>
     </head>
     <body>
     	<div id="group_item">
