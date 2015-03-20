0545f1f462311e90a8ff8f2f1e53302b95dccbd2
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-20 04:13:48 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop_show.php

--- 3/admin/info_shop_show.php
+++ 3/admin/info_shop_show.php
@@ -1,12 +1,3 @@
 <?php    
-    $arr = array(    
-        'name' => '陈毅鑫',    
-        'nick' => '深空',    
-        'contact' => array(    
-            'email' => 'shenkong at qq dot com',    
-            'website' => 'http://www.chinaz.com',    
-        )    
-    );    
-    $json_string = json_encode($arr);    
-    echo "getProfile($json_string)";    
+    echo $_GET['shop_name'];
 ?>  
