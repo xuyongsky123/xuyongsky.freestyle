87cdf86a4c6be190b5d638ff9e2c49ac785de029
xuyongsky123 <xuyongsky123@163.com>
2015-03-20 07:56:45 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop_show.php

--- admin/info_shop_show.php
+++ admin/info_shop_show.php
@@ -69,6 +69,9 @@
             <!-- Include all compiled plugins (below), or include individual files as needed -->
             <script src="../js/bootstrap_js/bootstrap.min.js"></script>
             <script src="./assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
+            <script src="./assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
+            <script src="./assets/js/docs.js" type="text/javascript"></script>
+            <script src="./assets/js/demo.js" type="text/javascript"></script>
         
     </body>
 </html>
