27139b694327e520cf479b417a86f4d65a61422b
xuyongsky123 <xuyongsky123@163.com>
2015-03-20 08:11:38 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_member_show.php

---
+++ admin/info_member_show.php
@@ -0,0 +1,52 @@
+<?php  
+	session_start();
+	header("Content-Type=text/html;charset=utf-8");
+    if(!isset($_SESSION['username_adminuserfixpassword__'])){
+        echo "<script language='javascript'>alert('Please login...');window.location.href='./index.php';</script>";
+    }else{
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        
+        <!--bootstrap_begin-->
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <!-- Bootstrap -->
+            <link href="../css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
+            
+            
+            <!-- Loading Flat UI -->
+            <link href="../css/bootstrap_css/flat_ui_css/flat-ui.css" rel="stylesheet" />
+        <!--bootstrap_end-->
+        
+        	<link href="./css/main_style.css" rel="stylesheet" />
+        
+        
+        <title>会员操作界面</title>
+        <style type="text/css">
+        </style>	
+    </head>
+    <body>
+        <div class="col-xs-12 col-xs-xuyong">
+            <nav class="navbar navbar-inverse navbar-embossed" role="navigation">
+                <div class="navbar-header navbar_xuyong">
+                  <a class="navbar-brand" href="#">Free Style</a>
+                </div>
+                <div class="collapse navbar-collapse nav_xuyong" id="navbar-collapse-01">
+                  <a id="back" href="./main.php">  
+                    <button id="menu-button" type="button" class="btn btn-default dropdown-toggle menu-button">
+                          返回
+                      </button>
+                  </a>
+                </div><!-- /.navbar-collapse -->
+            </nav><!-- /navbar -->
+        </div>
+      
+    </body>
+</html>
+<?php
+        
+    }
+?>
