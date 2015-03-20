5bc79ac68e140df97e37b77aeb5449db1dbeb199
xuyongsky123 <xuyongsky123@163.com>
2015-03-20 02:22:39 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_member.php

--- admin/info_member.php
+++ admin/info_member.php
@@ -2,56 +2,41 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
     <head>
-        <title>会员信息管理</title>
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-            <link href="../css/bootstrap_css/bootstrap.min.css" rel="stylesheet" />
-            
-            
-            <!-- Loading Flat UI -->
-            <link href="../css/bootstrap_css/flat_ui_css/flat-ui.css" rel="stylesheet" />
-            <link href="../css/bootstrap_css/flat_ui_css/demo.css" rel="stylesheet" />
-        <!--bootstrap_end-->
-        
-        <!-- Le styles -->
-        
-        	<link type="text/css" href="./css/jquery-ui-1.10.0.custom.css" rel="stylesheet" />
-        
-        	
-        	<link href="./css/main_style.css" rel="stylesheet" />
-        	
+        <style>
+            .group_content{
+                display:none;
+            }
+            .show{
+                display:block;
+            }
+        </style>
     </head>
     <body>
-        <script type="text/javascript">
-            $(function(){
-                $("#select_shop").bind("click",function(){
-                    alert("test");
-                });
-            });
-        </script>
-        <div id="tabs">
-            <ul>
-                <li><a href="#tabs-a">查询会员信息</a></li>
-                <li><a href="#tabs-b">修改会员信息</a></li>
-                <li><a href="#tabs-c">增加会员信息</a></li>
-                <li><a href="#tabs-d">删除会员信息</a></li>
+    	<div id="group_item">
+            <ul class="item_ul">
+                <li><a href="#tabs-a" class="item_click select_item" rel="select_item">查询会员信息</a></li>
+                <li><a href="#tabs-b" class="item_click modify_item" rel="modify_item">修改会员信息</a></li>
+                <li><a href="#tabs-c" class="item_click add_item" rel="add_item">增加会员信息</a></li>
+                <li><a href="#tabs-d" class="item_click delete_item" rel="delete_item">删除会员信息</a></li>
             </ul>
-            <div id="tabs-a">
-                <a href="#" id="select_shop">按钮<?php echo $_SESSION['username_adminuserfixpassword__']; ?></a>
-                <div id="select_content"></div>
+            <div id="action_content" class="action_content">
+            	<div id="select_item" class="group_content">
+                    <a href="http://www.baidu.com" id="select_shop">按钮<?php echo $_SESSION['username_adminuserfixpassword__']; ?></a>
+                    <div id="select_content"></div>
+            	</div>
+                <div id="modify_item" class="group_content">Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.....</div>
+                <div id="add_item" class="group_content">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
+                <div id="delete_item" class="group_content">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
             </div>
-            <div id="tabs-b">Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.....</div>
-            <div id="tabs-c">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
-            <div id="tabs-d">Nam dui era t, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.....</div>
         </div> 
         
-        <!-- Placed at the end of the document so the pages load faster -->
         <script src="../js/jquery-1.9.1.min.js"></script>
-        <script src="../js/bootstrap_js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="assets/js/jquery-ui-1.10.0.custom.min.js" type="text/javascript"></script>
-        <script src="assets/js/google-code-prettify/prettify.js" type="text/javascript"></script>
-        <script src="assets/js/docs.js" type="text/javascript"></script>
-        <script src="assets/js/demo.js" type="text/javascript"></script>
-    </body>    
-<html>
+        <script src="./js/action.js"></script>
+    </body>
+</html>
+        
+
+
+       
     
     
