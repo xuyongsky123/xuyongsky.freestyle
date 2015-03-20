8de7ddb909ebde071a75d70123a9657ac183702c
xuyongsky123 <xuyongsky123@163.com>
2015-03-20 05:54:22 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- admin/info_shop.php
+++ admin/info_shop.php
@@ -23,8 +23,8 @@
             <div id="action_content" class="action_content">
             	<div id="select_item" class="group_content">
                     <form id="search_form" onsubmit="getData();">
-                        <input name="action_identify" type="text" value="shop_select_item">
                     	<div class="search_container">
+                            <input name="action_identify" type="text" value="shop_select_item">
                             <input name="shop_name" type="text" placeholder="商店名称"/>
                         </div>
                         <div class="search_container">
