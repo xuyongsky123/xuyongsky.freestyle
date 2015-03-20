cd61af1a71a5e59565326e9949aa3b6a2a97573e
xuyongsky123 <xuyongsky123@163.com>
2015-03-20 06:25:30 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- admin/info_shop.php
+++ admin/info_shop.php
@@ -48,6 +48,22 @@
         
         <script src="../js/jquery-1.9.1.min.js"></script>
         <script src="./js/action.js"></script>
+        
+        <script type="text/javascript">
+            var loaded=false;
+            function getData(){
+                if(!loaded){
+                	alert("running");
+                    $("#search_form").submit();
+                    $.get("info_shop_show.php",$("#search_form").serialize(),
+                        function(data){
+                        alert(data);
+                    	}
+                    ); 
+                }
+                
+            }
+        </script>
     </body>
 </html>
         
