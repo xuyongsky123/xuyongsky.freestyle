3274250f6f715b77c7e802edb8947a14cbedba78
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-20 07:48:18 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop_show.php

--- 3/admin/info_shop_show.php
+++ 3/admin/info_shop_show.php
@@ -1,8 +1,4 @@
-<?php   
-header("Content-Type:text/html;charset=utf-8");
-if(!isset($_POST["shop_naem"]))
-    echo $_POST["shop_name"];
-?>  
+
 <?php  
 	session_start();
 	header("Content-Type=text/html;charset=utf-8");
