37bd39ffd9fb58885a7e2aa60e665c8272479fe2
xuyongsky123 <xuyongsky123@163.com>
2015-03-23 03:15:55 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- admin/info_shop.php
+++ admin/info_shop.php
@@ -117,7 +117,7 @@
                             if($array){
                                 foreach($array as $item){
                                     echo "<div>";
-                                    echo "<span><input name='shop[]' type='checkbox' value='".$item['shop_id']."'>".$item['shop_name']."</span>";
+                                    echo "<span><input name='shops[]' type='checkbox' value='".$item['shop_id']."'>".$item['shop_name']."</span>";
                                     echo "<span>".$item['shop_keeper']."</span>";
                                     echo "<span>".$item['shop_type']."</span>";
                                     echo "<span>".$item['shop_style_top']."</span>";
