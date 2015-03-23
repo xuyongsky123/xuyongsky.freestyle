9f484faed7c77eb4e1f9e5d4294d9a870129030e
xuyongsky123 <xuyongsky123@163.com>
2015-03-23 05:54:22 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_goods.php

--- admin/info_goods.php
+++ admin/info_goods.php
@@ -27,7 +27,62 @@
                 	开发中...
                 </div>
                 <div id="add_item" class="group_content">
-                    开发中...
+                    <form id="search_form" action="./info_shop_show.php" method="post">
+                        
+                        <input name="shop_identify" type="hidden" value="shop_add_item" />
+                        
+                    	<div class="search_container">
+                            <input name="shop_name" type="text" placeholder="商店名称"/>
+                        </div>
+                        <div class="search_container">
+                            <input name="shop_keeper" type="text" placeholder="店主名称"/>
+                        </div>
+                        <div class="search_container">
+                            <input name="goods_name" type="text" placeholder="商品名称"/>
+                        </div>
+                        <div class="search_container">
+                            <input name="goods_price" type="text" placeholder="商品价格"/>
+                        </div>
+                        <div class="search_container">
+                            <input name="goods_detail" type="text" placeholder="商品详情"/>
+                        </div>
+                        <div class="search_container">
+                            <label>商店展示上半部分背景</label>
+                            <select name="up_color">
+                                <option name="palette-turquoise" value="palette-turquoise">palette-turquoise</option>
+                                <option name="palette-emerald" value="palette-emerald">palette-emerald</option>
+                                <option name="palette-peter-river" value="palette-peter-river">palette-peter-river</option>
+                                <option name="palette-amethyst" value="palette-amethyst">palette-amethyst</option>
+                                <option name="palette-wet-asphalt" value="palette-wet-asphalt">palette-wet-asphalt</option>
+                                <option name="palette-sun-flower" value="palette-sun-flower">palette-sun-flower</option>
+                                <option name="palette-carrot" value="palette-carrot">palette-carrot</option>
+                                <option name="palette-alizarin" value="palette-alizarin">palette-alizarin</option>
+                                <option name="palette-clouds" value="palette-clouds">palette-clouds</option>
+                                <option name="palette-concrete" value="palette-concrete">palette-concrete</option>
+                            </select>
+                        </div>
+                        <div class="search_container">
+                            <label>商店展示下半部分背景</label>
+                            <select name="down_color">
+                                <option name="palette-green-sea" value="palette-green-sea">palette-green-sea</option>
+                                <option name="palette-nephritis" value="palette-nephritis">palette-nephritis</option>
+                                <option name="palette-belize-hole" value="palette-belize-hole">palette-belize-hole</option>
+                                <option name="palette-wisteria" value="palette-wisteria">palette-wisteria</option>
+                                <option name="palette-midnight-blue" value="palette-midnight-blue">palette-midnight-blue</option>
+                                <option name="palette-orange" value="palette-orange">palette-orange</option>
+                                <option name="palette-pumpkin" value="palette-pumpkin">palette-pumpkin</option>
+                                <option name="palette-pomegranate" value="palette-pomegranate">palette-pomegranate</option>
+                                <option name="palette-silver" value="palette-silver">palette-silver</option>
+                                <option name="palette-asbestos" value="palette-asbestos">palette-asbestos</option>
+                            </select>
+                        </div>
+                        <div class="search_container">
+                            <input name="goods_detail" type="text" placeholder="商品图片地址（URL）"/>
+                        </div>
+                        <div class="search_container">
+                            <input name="submit" type="submit" value="添加商品"/>
+                        </div>
+                    </form>
                 </div>
                 <div id="delete_item" class="group_content">
                     开发中...
