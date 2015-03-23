05bd3386f5bb8b7bba8e66c1a00c365a939c8f03
xuyongsky123 <xuyongsky123@163.com>
2015-03-23 01:29:41 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- admin/info_shop.php
+++ admin/info_shop.php
@@ -59,6 +59,24 @@
                             <input name="shop_type" type="text" placeholder="商店类型名称"/>
                         </div>
                         <div class="search_container">
+                            <label>商店展示上半部分背景</label>
+                            <select name="pice">
+                                <option name="clothes" value="上衣">上衣</option>
+                                <option name="shoes" value="鞋子">鞋子</option>
+                                <option name="socket" value="袜子">袜子</option>
+                                <option name="trousers" value="裤子">裤子</option>
+                            </select><br/>
+                        </div>
+                        <div class="search_container">
+                            <label>商店展示下半部分背景</label>
+                            <select name="pice">
+                                <option name="clothes" value="上衣">上衣</option>
+                                <option name="shoes" value="鞋子">鞋子</option>
+                                <option name="socket" value="袜子">袜子</option>
+                                <option name="trousers" value="裤子">裤子</option>
+                            </select><br/>
+                        </div>
+                        <div class="search_container">
                             <input name="submit" type="submit" value="添加商店"/>
                         </div>
                     </form>
