33a9872245da7e61fbd3bd6b9444a46abc86794d
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-03-23 02:21:47 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop_show.php

--- 3/admin/info_shop_show.php
+++ 3/admin/info_shop_show.php
@@ -49,6 +49,73 @@
         		$identify=$_POST['shop_identify'];
                 if(!isset($_POST['submit'])){
                     echo "非法提交数据，请以正确方式提交...";
+                }else{
+                	if(!isset($identify))
+                    {
+                        echo "<script type='text/javascript'>alert('数据操作失败，请重新操作...');window.location.href='./main.php';</script>";
+                    }else if($identify=='shop_select_item'){
+                        $shop_name=$_POST['shop_name'];
+                        $shop_keeper=$_POST['shop_keeper'];
+                        $shop_type=$_POST['shop_type'];
+                        $submit_value=$_POST['submit'];
+                        
+                        if($submit_value=="商店查询"){
+                            $sql="select * from shop_info where shop_name = '$shop_name' or shop_keeper= '$shop_keeper' or shop_type= '$shop_type' ";
+                            $array=$operatedb->Execsql($sql,$conn);
+                            
+                         
+                            if($array){
+                                foreach($array as $item){
+                                    echo "<div>";
+                                    echo "<span>".$item['shop_name']."</span>";
+                                    echo "<span>".$item['shop_keeper']."</span>";
+                                    echo "<span>".$item['shop_type']."</span>";
+                                    echo "<span>".$item['shop_style_top']."</span>";
+                                    echo "<span>".$item['shop_style_bottom']."</span>";
+                                    echo "</div>";
+                                }
+                            }else{
+                                    echo "没有查到相关商店信息...";
+                            }
+                        }else{
+                            $sql="select * from shop_info";
+                            $array=$operatedb->Execsql($sql,$conn);
+                            
+                            
+                            if($array){
+                                foreach($array as $item){
+                                    echo "<div>";
+                                    echo "<span>".$item['shop_name']."</span>";
+                                    echo "<span>".$item['shop_keeper']."</span>";
+                                    echo "<span>".$item['shop_type']."</span>";
+                                    echo "<span>".$item['shop_style_top']."</span>";
+                                    echo "<span>".$item['shop_style_bottom']."</span>";
+                                    echo "</div>";
+                                }
+                            }else{
+                                    echo "没有查到相关商店信息...";
+                            }
+                        }
+                        
+                        
+                    }else if($identify=='shop_add_item'){
+                        $shop_name=$_POST['shop_name'];
+                        $shop_keeper=$_POST['shop_keeper'];
+                        $shop_type=$_POST['shop_type'];
+                        $shop_upcolor=$_POST['up_color'];
+                        $sho_downcolor=$_POST['down_color'];
+                        
+                        
+                            $sql="insert into shop_info (shop_name,shop_keeper,shop_type,shop_style_top,shop_style_bottom) values ('$shop_name','$shop_keeper','$shop_type','$shop_upcolor','$sho_downcolor') ";
+                            $result=$operatedb->Execsql($sql,$conn);
+                
+                            if($result){
+                                echo "<script language='javascript'>alert('添加商店成功');history.back();</script>";
+                            }else{
+                                echo "<script language='javascript'>alert('添加商店失败，请重新添加...');history.back();</script>";	
+                            }
+                       
+                    }
                 }
             ?>
         </div>
