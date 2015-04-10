fb0064bc64747108e914cebc5c969d54c356bcfc
xuyongsky123 <xuyongsky123@163.com>
2015-04-10 05:14:33 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/index.php

--- index.php
+++ index.php
@@ -109,6 +109,7 @@
 		<div id="contact_content">
 			<a target="blank" href="http://wpa.qq.com/msgrd?v=3&uin=1845082157&site=qq&menu=yes"><img border="0" src=http://wpa.qq.com/pa?p=1:1845082157:51 alt="点击这里给我发消息"></a>
 			<a target="blank" href="http://wpa.qq.com/msgrd?v=3&uin=821594487&site=qq&menu=yes"><img border="0" src=http://wpa.qq.com/pa?p=1:821594487:51 alt="点击这里给我发消息"></a>
+            <a target="blank" href="http://wpa.qq.com/msgrd?v=3&uin=821594487&site=qq&menu=yes"><img border="0" src=http://wpa.qq.com/pa?p=1:910259041:51 alt="点击这里给我发消息"></a>
 		</div>
 	</div>
 	<div id="user_container">
