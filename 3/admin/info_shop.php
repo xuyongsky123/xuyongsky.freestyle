4af6ad510741ffa2bd06d500f0ba5af85e6ed78b
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-05-24 05:53:54 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/admin/info_shop.php

--- 3/admin/info_shop.php
+++ 3/admin/info_shop.php
@@ -15,10 +15,10 @@
     <body>
     	<div id="group_item">
             <ul class="item_ul">
-                <li><a href="#tabs-a" class="item_click select_item" rel="select_item">查询商店信息</a></li>
-                <li><a href="#tabs-b" class="item_click modify_item" rel="modify_item">修改商店信息</a></li>
-                <li><a href="#tabs-c" class="item_click add_item" rel="add_item">增加商店信息</a></li>
-                <li><a href="#tabs-d" class="item_click delete_item" rel="delete_item">删除商店信息</a></li>
+                <li><a href="#tabs-a" class="item_click select_item" rel="select_item">查询产品类型信息</a></li>
+                <li><a href="#tabs-b" class="item_click modify_item" rel="modify_item">修改产品类型信息</a></li>
+                <li><a href="#tabs-c" class="item_click add_item" rel="add_item">增加产品类型信息</a></li>
+                <li><a href="#tabs-d" class="item_click delete_item" rel="delete_item">删除产品类型信息</a></li>
             </ul>
             <div id="action_content" class="action_content">
             	<div id="select_item" class="group_content">
