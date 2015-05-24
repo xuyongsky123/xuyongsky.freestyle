7b948e40b85bdfbdbc730eb8535c6c6ce0d9599f
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-05-24 05:45:31 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_shop.php

--- 3/module_shop_shop.php
+++ 3/module_shop_shop.php
@@ -15,7 +15,7 @@
         <div class="col-xs-3">
           <div class="todo">
             <div class="todo-search">
-              <input class="todo-search-field" type="search" value="" readonly="readonly" placeholder="店铺类型" />
+              <input class="todo-search-field" type="search" value="" readonly="readonly" placeholder="产品类型" />
             </div>
             <ul>
                 <?php include("./module_shop_showshoptypelist.php"); ?>
