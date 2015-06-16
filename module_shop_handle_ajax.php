824b6305ff4a3deb31cee68096ba003da20dd97d
xuyongsky123 <xuyongsky123@163.com>
2015-06-16 13:43:58 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_handle_ajax.php

--- module_shop_handle_ajax.php
+++ module_shop_handle_ajax.php
@@ -1,12 +1,24 @@
 <?php
     require("./database/connDB.inc.php");
-
-    $sql_search="insert into goods_cart (goods_id,user_id) values (".$_REQUEST['goods_id'].",".$_REQUEST['user_id'].")";
-    $array_search=$operatedb->Execsql($sql_search,$conn);
-
-    if($array_search){
-        return true;
-    }else{
-        return false;
+	if($_REQUEST['action']=="addToCart"){
+		$sql_search="insert into goods_cart (goods_id,user_id) values (".$_REQUEST['goods_id'].",".$_REQUEST['user_id'].")";
+        $array_search=$operatedb->Execsql($sql_search,$conn);
+    
+        if($array_search){
+            return true;
+        }else{
+            return false;
+        }
+    }else($_REQUEST['action']=="deleteFromCart"){
+    	$sql_search="delete from goods_cart where user_id=".$_REQUEST['user_id']." and goods_id=".$_REQUEST['goods_id'];
+        $array_search=$operatedb->Execsql($sql_search,$conn);
+    
+        if($array_search){
+            return true;
+        }else{
+            return false;
+        }
     }
+
+    
 ?>
