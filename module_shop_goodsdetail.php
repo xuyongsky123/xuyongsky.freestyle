6f8fd1162b5f60986a1c1a69ea6a95c178d835d3
xuyongsky123 <xuyongsky123@163.com>
2015-06-16 09:36:30 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_goodsdetail.php

--- module_shop_goodsdetail.php
+++ module_shop_goodsdetail.php
@@ -29,8 +29,8 @@
                             echo '<div class="detail-name-xy">'.$item_search["goods_name"].'</div>';
                             echo '<div class="detail-price-xy">'.$item_search["goods_price"].'</div>';
                             echo '<div class="detail-detail-xy">'.$item_search["goods_detail"].'</div>';
-                            echo '<a class="detail-cart">加入购物车</a>';
-                            echo '<a class="detail-buy">立即购买</a>';
+                            echo '<a class="col-xs-2 detail-cart">加入购物车</a>';
+                            echo '<a class="col-xs-2 detail-buy">立即购买</a>';
                             echo '</div>';
                         }
                     }else{
