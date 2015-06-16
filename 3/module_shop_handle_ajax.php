9aded6604ed36b90086d28a5b460a29fb2e0572b
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-06-16 11:47:31 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_handle_ajax.php

--- 3/module_shop_handle_ajax.php
+++ 3/module_shop_handle_ajax.php
@@ -1,7 +1,7 @@
 <?php
 	echo $_REQUEST['goods_id'];
     $shop_search=$_GET['id'];
-    $sql_search="insert into goods_cart (id,user_id) values (".$_REQUEST['goods_id'].",".$_REQUEST['user_id'].")";
+    $sql_search="insert into goods_cart (goods_id,user_id) values (".$_REQUEST['goods_id'].",".$_REQUEST['user_id'].")";
     $array_search=$operatedb->Execsql($sql_search,$conn);
 	return $_REQUEST['goods_id'];
 ?>
