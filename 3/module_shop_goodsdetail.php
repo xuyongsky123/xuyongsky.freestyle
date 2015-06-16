bf6142664a0ba33deeee7402be589ccbc40030b9
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-06-16 09:38:39 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_goodsdetail.php

--- 3/module_shop_goodsdetail.php
+++ 3/module_shop_goodsdetail.php
@@ -23,12 +23,15 @@
                     if($array_search){
                         foreach($array_search as $item_search){
                             echo '<div class="col-xs-5">';
+                            echo '<div class="col-xs-1"></div>';
                             echo '<img class="detail-style" src="'.$item_search["goods_detail_image_url"].'">';
                             echo '</div>';
                             echo '<div class="col-xs-7">';
                             echo '<div class="detail-name-xy">'.$item_search["goods_name"].'</div>';
                             echo '<div class="detail-price-xy">'.$item_search["goods_price"].'</div>';
+                            echo '<div class="col-xs-1"></div>';
                             echo '<div class="detail-detail-xy">'.$item_search["goods_detail"].'</div>';
+                            echo '<div class="col-xs-1"></div>';
                             echo '<div class="col-xs-1"></div>';
                             echo '<a class="col-xs-2 detail-cart">加入购物车</a>';
                             echo '<div class="col-xs-1"></div>';
