cc5bcce4de20ee1d7c901d961ebf8ce4cb910dcf
xuyongsky123 <xuyongsky123@163.com>
2015-06-16 11:49:17 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_handle_ajax.php

--- module_shop_handle_ajax.php
+++ module_shop_handle_ajax.php
@@ -1,4 +1,6 @@
 <?php
+    require("./database/connDB.inc.php");
+
 	echo $_REQUEST['goods_id'];
     $shop_search=$_GET['id'];
     $sql_search="insert into goods_cart (goods_id,user_id) values (".$_REQUEST['goods_id'].",".$_REQUEST['user_id'].")";
