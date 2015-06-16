6e28e1f1ac52a469fba5a7fc435212c9e96125e3
xuyongsky123 <xuyongsky123@163.com>
2015-06-16 13:07:44 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/goods_cart.php

--- goods_cart.php
+++ goods_cart.php
@@ -20,7 +20,7 @@
                     if($array_search){
                         foreach($array_search as $item_search){
                             echo '<div class="col-xs-3"></div>';
-                            echo '<div class="col-xs-6">'.$item_search["goods_id"].'test</div>';
+                            echo '<div class="col-xs-6">'.$item_search["goods_id"].'</div>';
                             echo '<div class="col-xs-3"></div>';
                         }
                     }else{
