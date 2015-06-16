beb493084ac9e33cd53968a87f0b0bffa174d31c
xuyongsky123 <xuyongsky123@163.com>
2015-06-16 12:00:04 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_handle_ajax.php

--- module_shop_handle_ajax.php
+++ module_shop_handle_ajax.php
@@ -3,6 +3,7 @@
 
     $sql_search="insert into goods_cart (goods_id,user_id) values (".$_REQUEST['goods_id'].",".$_REQUEST['user_id'].")";
     $array_search=$operatedb->Execsql($sql_search,$conn);
+
     if($array_search){
         return true;
     }else{
