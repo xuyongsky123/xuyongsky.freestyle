bdd6e883e5ea57fd9a43df0e520aa065360b7b75
xuyongsky123@163.com <xuyongsky123@163.com@659b38d1-5c1f-4742-8b09-a24be1545116>
2015-06-16 13:51:26 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_handle_ajax.php

--- 3/module_shop_handle_ajax.php
+++ 3/module_shop_handle_ajax.php
@@ -11,6 +11,8 @@
         }
     }else($_REQUEST['action']=="deleteFromCart"){
     	$sql_search="delete from goods_cart where user_id=".$_REQUEST['user_id']." and goods_id=".$_REQUEST['goods_id'];
+        var_dump($sql_search);
+        die;
         $array_search=$operatedb->Execsql($sql_search,$conn);
     
         if($array_search){
