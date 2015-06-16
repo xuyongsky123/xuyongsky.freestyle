729ba3efce98551966138051de4515415a6f48ad
xuyongsky123 <xuyongsky123@163.com>
2015-06-16 13:13:14 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/goods_cart.php

--- goods_cart.php
+++ goods_cart.php
@@ -19,9 +19,8 @@
                     $array_search=$operatedb->Execsql($sql_search,$conn);
                     if($array_search){
                         foreach($array_search as $item_search){
-                            echo '<div class="col-xs-4"></div>';
-                            echo '<div class="col-xs-4"><a href="module_shop_goodsdetail.php?id='.$item_search["goods_id"].'">'.$item_search["goods_id"].'</a></div>';
-                            echo '<div class="col-xs-4">删除</div>';
+                            echo '<div class="col-xs-3"></div>';
+                            echo '<div class="col-xs-9"><a href="module_shop_goodsdetail.php?id='.$item_search["goods_id"].'">'.$item_search["goods_id"].'</a></div>';
                         }
                     }else{
                         echo '<div>';
