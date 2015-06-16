12d3805329a0e612f0ea825d60b30054408fcc77
xuyongsky123 <xuyongsky123@163.com>
2015-06-16 10:28:01 +0000

    Autoversioning commit:  a non-deltaV client made a change to /3/module_shop_goodsdetail.php

--- module_shop_goodsdetail.php
+++ module_shop_goodsdetail.php
@@ -8,6 +8,11 @@
     <?php include('./frame_bootstrap_templethead.php'); ?>
     <link href="./css/module_shop_css/module_shop_style.css" rel="stylesheet" />
 	<title><?php echo $_GET["shop_name"]; ?></title>
+      <script>
+          function addToCart(){
+          	
+          }
+      </script>
   </head>
   <body>
       <?php include("./module_shop_templetheader.php"); ?>
